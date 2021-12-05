047063aeff4d32dfeb2d6eee20f39aafe0c78f15
RyanB1303 <popOs>
2021-12-05 21:22:06 +0700

    add koefisiens dynamicly seeds now remove all record first

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Lembaga.destroy_all
+Pajak.destroy_all
 Lembaga.create(nama_lembaga: 'Kota Madiun', tahun: '2021')
 Pajak.create(tahun: "2021", tipe: "Tanpa Pajak", potongan: 0)
 Pajak.create(tahun: "2021", tipe: "PPN", potongan: 0.1)
