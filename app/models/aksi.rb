02c47db11f5ffd112ee411ade849079f6db0308b
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2021-12-07 14:27:29 +0700

    refactor sedikit rincian sasaran

--- app/models/aksi.rb
+++ app/models/aksi.rb
@@ -2,10 +2,16 @@ class Aksi < ApplicationRecord
   belongs_to :tahapan
   after_save :update_total_target_bulan
   after_save :update_total_realisasi_bulan
+  after_save :update_waktu
+  after_save :update_progress
   after_update :update_total_target_bulan
   after_update :update_total_realisasi_bulan
+  after_update :update_waktu
+  after_update :update_progress
   after_destroy  :update_total_target_bulan
   after_destroy  :update_total_realisasi_bulan
+  after_destroy  :update_waktu
+  after_destroy  :update_progress
   
 
   
@@ -18,6 +24,25 @@ class Aksi < ApplicationRecord
   def update_total_realisasi_bulan
     tahapan = self.tahapan
     tahapan.jumlah_realisasi = tahapan.aksis.sum :realisasi
+    tahapan.save
+  end
+
+  def update_waktu
+    tahapan = self.tahapan
+    tahapan.waktu = tahapan.aksis.count(:bulan)
+    tahapan.save
+  end
+
+  def update_progress
+    tahapan = self.tahapan
+    t= tahapan.jumlah_target
+    r= tahapan.jumlah_realisasi
+    check = r * t
+    if check != 0
+      tahapan.progress = r / t * 100
+    else
+      tahapan.progress = 0
+    end
     tahapan.save
   end
 
--- app/models/rincian.rb
+++ app/models/rincian.rb
@@ -3,4 +3,31 @@ class Rincian < ApplicationRecord
   has_many :kesenjangan
   has_many :tahapans
   accepts_nested_attributes_for :tahapans
+
+  def target_bulan
+    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:target)}
+  end
+
+  def realisasi_bulan
+    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:realisasi)}
+  end
+
+  def total_target_aksi_bulan
+    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:target)}.inject{|bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v} }
+  end
+
+  def total_realisasi_aksi_bulan
+    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:realisasi)}.inject{|bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v} }
+  end
+
+  def waktu_total
+    self.total_target_aksi_bulan.count
+  end
+
+  def progress_total
+    jumlah_target = self.tahapans.sum :jumlah_target
+    jumlah_realisasi = self.tahapans.sum :jumlah_realisasi
+    ((jumlah_realisasi.to_f / jumlah_target.to_f ) * 100) rescue 0
+  end
+
 end
