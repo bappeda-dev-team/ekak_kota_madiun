f9e02139425a57ba4ea554456b5a49fd8377f89f
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2021-12-07 08:09:02 +0700

    modal perhitungan dan koefisien sudah bener

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
-  def add_koefisiens(name, f, association)
+  def add_koefisiens(name, f, association, dc)
     ## create new object from the association
+    ## dc == dynamic class for css
     new_object = f.object.send(association).klass.new
 
     ## create or take id from the new created obejct
@@ -9,7 +10,7 @@ module ApplicationHelper
       render(association.to_s.singularize + "_fields", f: builder)
       end
     ## pass down the link to the fields form
-    link_to(name, '#', class: 'add_fields', data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(name, '#', class: "add_fields_#{dc}", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
 end
