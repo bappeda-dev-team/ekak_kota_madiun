cdd6cb3bfe7accd406c83966a44323a2b86fbbc6
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2021-12-09 14:41:15 +0700

    idk what to do after this plz find good design and decide to go there

--- app/models/rincian.rb
+++ app/models/rincian.rb
@@ -8,25 +8,26 @@ class Rincian < ApplicationRecord
     return 0
   end
 
+  # method yang ada map nya memang sengaja begitu, karena dibuat collection dan di loop untuk footer bulan
   def target_bulan
-    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:target)}
+    self.tahapans.map { |t| t.aksis.group(:bulan).sum(:target) }
   end
 
   def realisasi_bulan
-    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:realisasi)}
+    self.tahapans.map { |t| t.aksis.group(:bulan).sum(:realisasi) }
   end
 
   def total_target_aksi_bulan
-    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:target)}.inject{|bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v} }
+    self.tahapans.map { |t| t.aksis.group(:bulan).sum(:target) }.inject { |bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v } }
   end
 
   def total_realisasi_aksi_bulan
-    self.tahapans.map{|t| t.aksis.group(:bulan).sum(:realisasi)}.inject{|bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v} }
+    self.tahapans.map { |t| t.aksis.group(:bulan).sum(:realisasi) }.inject { |bln, val| bln.merge(val) { |k, old_v, new_v| old_v + new_v } }
   end
 
   def waktu_total
     begin
-    self.total_target_aksi_bulan.count
+      self.total_target_aksi_bulan.count
     rescue NoMethodError => e
       print_exception(e, true)
     end
@@ -35,11 +36,10 @@ class Rincian < ApplicationRecord
   def progress_total
     jumlah_target = self.tahapans.sum :jumlah_target
     jumlah_realisasi = self.tahapans.sum :jumlah_realisasi
-    ((jumlah_realisasi.to_f / jumlah_target.to_f ) * 100) rescue 0
+    ((jumlah_realisasi.to_f / jumlah_target.to_f) * 100) rescue 0
   end
 
   def total_anggaran
-    self.tahapans.map{ |t| t.anggarans.find_by(level: 0)}.sum{|n| n.jumlah}
+    self.tahapans.map { |t| t.anggarans.find_by(level: 0) }.sum { |n| n.jumlah }
   end
-
 end
--- app/models/sasaran.rb
+++ app/models/sasaran.rb
@@ -2,5 +2,4 @@ class Sasaran < ApplicationRecord
   belongs_to :user
   has_one :rincian
   belongs_to :program_kegiatan, optional: true
-
 end
