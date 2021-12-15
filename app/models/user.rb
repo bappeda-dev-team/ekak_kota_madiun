24c10ba98671b5e4177e555b1ca93f150b319df9
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2021-12-15 16:34:25 +0700

    simple edit user dan link

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # validates :nama, presence: true
   # validates :nik, presence: true
-  # belongs_to :opd
+  belongs_to :opd
   has_many :pks
   has_many :kaks
   has_many :sasarans, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+  root to: 'home#dashboard'
   get 'dashboard', to: 'home#dashboard'
 
   # resources :perhitungans
