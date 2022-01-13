f22b63497c1cbcd7eb873ad2f757d77d0505e549
Ryan B <ryan.brilliant.nirwana@gmail.com>
2022-01-13 23:01:37 +0700

    fixing docker env fixing rincian sasaran view

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,17 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  // $: 'jquery',
-  // jQuery: 'jquery',
-  // Popper: ['popper.js', 'default'],
-  // Swal: 'sweetalert2',
-  // SmoothScroll: 'smooth-scroll',
-  // bootstrap: 'bootstrap',
-  // Chartist: 'chartist'
-})
-)
+environment.plugins.append(
+	"Provide",
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		// jQuery: 'jquery',
+		// Popper: ['popper.js', 'default'],
+		// Swal: 'sweetalert2',
+		// SmoothScroll: 'smooth-scroll',
+		// bootstrap: 'bootstrap',
+		// Chartist: 'chartist'
+	})
+);
 
-module.exports = environment
+module.exports = environment;
