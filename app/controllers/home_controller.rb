043d827c165137b74262c162d28ee20b01d8f760
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-01-19 15:41:04 +0700

    add script for dashboard statistic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def dashboard
     @sasaran = current_user.sasarans.all
+    @total_progress = @sasaran.map{ |s| s.rincian.progress_total  }.sum
+    @avg_progress = @total_progress / @sasaran.count
     render 'dashboard'
   end
 end
