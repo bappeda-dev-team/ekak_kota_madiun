75797102ed6b39351bbaa27ad0f6637ca997d35f
RyanB1303 <popOs>
2022-01-19 21:59:29 +0700

    dashboard grafik work refactor to model new method for user longest working time fffff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
 
   def dashboard
     @sasaran = current_user.sasarans.all
-    @total_progress = @sasaran.map{ |s| s.rincian.progress_total  }.sum
+    @total_progress = current_user.total_user_progress
     @avg_progress = @total_progress / @sasaran.count
+    @chart = current_user.generate_chart_value
+    @chart_real = current_user.generate_chart_value_realisasi
     render 'dashboard'
   end
 end
