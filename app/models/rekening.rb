2bea9682a3198dd84a52cedffac1c4ae66db0f0c
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-02-26 22:43:29 +0700

    determine rekening level and parent and grand parent spec for rekening

--- app/models/rekening.rb
+++ app/models/rekening.rb
@@ -10,4 +10,40 @@
 #  updated_at     :datetime         not null
 #
 class Rekening < ApplicationRecord
+  def grand_parent
+    kode_rekening = self.kode_rekening
+    hasil = Rekening.find_by(kode_rekening: kode_rekening[0..2])
+  end
+
+  def parent
+    # TODO : tambah exception untuk NilClass kode_rekening
+    kode_rekening = self.kode_rekening
+    if self.level == 4
+      hasil = Rekening.find_by(kode_rekening: kode_rekening[0..-6])
+    elsif self.level == 3
+      hasil = Rekening.find_by(kode_rekening: kode_rekening[0..-4])
+    elsif self.level == 2
+      hasil = Rekening.find_by(kode_rekening: kode_rekening[0..-4])
+    elsif self.level == 1
+      hasil = Rekening.find_by(kode_rekening: kode_rekening[0..-4])
+    else
+      hasil = self.grand_parent
+    end
+    return hasil
+  end
+
+  def level
+    panjang_kode_rek = self.kode_rekening.length
+    if panjang_kode_rek <= 3
+      return 0
+    elsif panjang_kode_rek <= 6
+      return 1
+    elsif panjang_kode_rek <= 9
+      return 2
+    elsif panjang_kode_rek <= 12
+      return 3
+    else
+      return 4
+    end
+  end
 end
--- spec/models/rekening_spec.rb
+++ spec/models/rekening_spec.rb
@@ -9,8 +9,92 @@
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Rekening, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def rekening_level_4
+    Rekening.create(
+      kode_rekening: "5.1.02.02.08.0026",
+      jenis_rekening: "Parent Level 4",
+    )
+  end
+
+  def rekening_level_3
+    Rekening.create(
+      kode_rekening: "5.1.02.02.08",
+      jenis_rekening: "Parent Level 3",
+    )
+  end
+
+  def rekening_level_2
+    Rekening.create(
+      kode_rekening: "5.1.02.02",
+      jenis_rekening: "Parent Level 2",
+    )
+  end
+
+  def rekening_level_1
+    Rekening.create(
+      kode_rekening: "5.1.02",
+      jenis_rekening: "Parent Level 1",
+    )
+  end
+
+  def rekening_level_0
+    Rekening.create(
+      kode_rekening: "5.1",
+      jenis_rekening: "Grand Parent",
+    )
+  end
+
+  context "Rekening#parent" do
+    it "can determine level 4 parent" do
+      rek_parent = rekening_level_3
+      rek_sekarang = rekening_level_4
+      parent = rek_sekarang.parent
+      expect(parent).to eq(rek_parent)
+    end
+    it "can determine level 3 parent" do
+      rek_parent = rekening_level_2
+      rek_sekarang = rekening_level_3
+      parent = rek_sekarang.parent
+      expect(parent).to eq(rek_parent)
+    end
+    it "can determine level 2 parent" do
+      rek_parent = rekening_level_1
+      rek_sekarang = rekening_level_2
+      parent = rek_sekarang.parent
+      expect(parent).to eq(rek_parent)
+    end
+
+    it "can determine level 1 parent" do
+      rek_parent = rekening_level_0
+      rek_sekarang = rekening_level_1
+      parent = rek_sekarang.parent
+      expect(parent).to eq(rek_parent)
+    end
+  end
+
+  context "Rekening#grand_parent" do
+    it "can determine the grand_parent from any position" do
+      rek_grand_parent = rekening_level_0
+      rek_sekarang = rekening_level_4
+      grand_parent = rek_sekarang.grand_parent
+      expect(grand_parent).to eq(rek_grand_parent)
+    end
+  end
+
+  context "Rekening#level" do
+    it "can determine level 4" do
+      rekening = rekening_level_4
+      level = rekening.level
+      expect(level).to eq(4)
+    end
+    it "can determine any level" do
+      expect(rekening_level_2.level).to eq(2)
+      expect(rekening_level_1.level).to eq(1)
+      expect(rekening_level_3.level).to eq(3)
+      expect(rekening_level_0.level).to eq(0)
+    end
+  end
 end
