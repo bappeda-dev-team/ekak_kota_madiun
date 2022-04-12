9d3777386650215f9d372dfd0f0f2c5daf52f9f0
PC-Kantor <ryan.brilliant.nirwana@gmail.com>
2022-04-12 09:16:24 +0700

    fixing add musrenbang

--- app/controllers/usulans_controller.rb
+++ app/controllers/usulans_controller.rb
@@ -1,11 +1,15 @@
 class UsulansController < ApplicationController
   def update_sasaran_asn
-    sasaran = Sasaran.find params[:sasaran_id]
+    sasaran = params[:sasaran_id]
     usulan = params[:usulan_id].to_i
     usulan_type = params[:usulan_type]
-    u = Usulan.create(usulanable_id: usulan, usulanable_type: usulan_type, sasaran: sasaran)
+    u = Usulan.create(usulanable_id: usulan, usulanable_type: usulan_type, sasaran_id: sasaran)
     respond_to do |format|
-      format.js { render 'musrenbangs/update_sasaran_asn' } if u.save
+      if u.save
+        format.js { render 'musrenbangs/update_sasaran_asn' }
+      else
+        format.js { render 'musrenbangs/update_sasaran_asn', status: :unprocessable_entity }
+      end
     end
   end
 end
