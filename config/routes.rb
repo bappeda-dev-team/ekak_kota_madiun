1963dd70a16da3e1b7a6969e7e2a26a99d3ab226
RyanB <ryan.brilliant.nirwana@gmail.com>
2022-05-16 15:17:52 +0700

    rab untuk admin

--- config/routes.rb
+++ config/routes.rb
@@ -172,6 +172,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   post '/filter_user', to: 'filter#filter_user'
   post '/filter_program', to: 'filter#filter_program'
   post '/filter_kak', to: 'filter#filter_kak'
+  post '/filter_rab', to: 'filter#filter_rab'
   # get "/program_kegiatans", to: "program_kegiatans#index"
   # get "/program_kegiatans/new", to: "program_kegiatans#new"
   # get "/program_kegiatan/:id", to: "program_kegiatans#show"
