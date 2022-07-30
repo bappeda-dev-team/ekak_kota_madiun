b616f2feb30ccbca125e7ec17d2c0ff28b86ce9b
ry@pop_os <ryan.brilliant.nirwana@gmail.com>
2022-07-30 12:32:08 +0700

    basic spec

--- spec/factories/kelompok_anggarans.rb
+++ spec/factories/kelompok_anggarans.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: kelompok_anggarans
+#
+#  id            :bigint           not null, primary key
+#  kelompok      :string
+#  kode_kelompok :string
+#  tahun         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 FactoryBot.define do
   factory :kelompok_anggaran do
     kode_kelompok { "MyString" }
--- spec/models/kelompok_anggaran_spec.rb
+++ spec/models/kelompok_anggaran_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe KelompokAnggaran, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'valid dengan kelompok Awal dan Perubahan' do
+      ka = KelompokAnggaran.create(kelompok: 'Awal', tahun: 2022, kode_kelompok: '_awal')
+      kp = KelompokAnggaran.create(kelompok: 'Perubahan', tahun: 2022, kode_kelompok: '_perubahan')
+      expect(ka).to be_valid
+      expect(kp).to be_valid
+    end
+
+    it 'tidak valid dengan tahun selain angka' do
+      ka = KelompokAnggaran.create(kelompok: 'Awal', tahun: 'zzz', kode_kelompok: '_awal')
+      expect(ka).to be_invalid
+    end
+
+    it 'tidak valid dengan duplikat tahun dan kode yang sama' do
+      KelompokAnggaran.create(kelompok: 'Awal', tahun: 2022, kode_kelompok: '_awal')
+      ka2 = KelompokAnggaran.create(kelompok: 'Awal', tahun: 2022, kode_kelompok: '_awal')
+      expect(ka2).to be_invalid
+    end
+  end
+
+  describe 'pembuat kode' do
+    it 'membuat kode kelompok dengan pola _awal' do
+      ka = KelompokAnggaran.create(kelompok: 'Awal', tahun: 2022)
+      expect(ka.kode_kelompok).to eq('_awal')
+    end
+
+    it 'membuat kode kelompok dengan pola _perubahan' do
+      ka = KelompokAnggaran.create(kelompok: 'Perubahan', tahun: 2022)
+      expect(ka.kode_kelompok).to eq('_perubahan')
+    end
+
+    context 'lewat validasi' do
+      it 'tidak valid jika bukan Awal atau Perubahan' do
+        ka2 = KelompokAnggaran.create(kelompok: 'Sendiri', tahun: 2022, kode_kelompok: '_perubahan')
+        expect(ka2).to be_invalid
+      end
+    end
+  end
 end
