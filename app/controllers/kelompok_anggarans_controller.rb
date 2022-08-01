fe6200d194a99ad5061c55bc7b17e93a27bd2006
PC-Kantor <ryan.brilliant.nirwana@gmail.com>
2022-08-01 07:19:23 +0700

    fix kelompok_anggaran not showing

--- app/controllers/kelompok_anggarans_controller.rb
+++ app/controllers/kelompok_anggarans_controller.rb
@@ -3,10 +3,7 @@ class KelompokAnggaransController < ApplicationController
 
   # GET /kelompok_anggarans or /kelompok_anggarans.json
   def index
-    @kelompok_anggarans = KelompokAnggaran.all.to_a
-    (2022..2024).each do |tahun|
-      @kelompok_anggarans << OpenStruct.new(kode_tahun_sasaran: tahun, tahun_kelompok: tahun)
-    end
+    @kelompok_anggarans = KelompokAnggaran.all
   end
 
   def cloning
