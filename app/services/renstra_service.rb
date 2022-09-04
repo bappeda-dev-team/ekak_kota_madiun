6e15b9bf379a50aaee91be412b5cb26746db4db9
ry@pop_os <ryan.brilliant.nirwana@gmail.com>
2022-09-05 00:54:15 +0700

    spec for renstra_service

---
+++ app/services/renstra_service.rb
@@ -0,0 +1,36 @@
+# return renstra by opd and program
+class RenstraService
+  attr_accessor :kode_program, :kode_unik_opd, :tahun
+
+  def initialize(kode_unik_opd:, kode_program: nil, tahun: nil)
+    @kode_unik_opd = kode_unik_opd
+    @kode_program = kode_program
+    @tahun = tahun
+    @opd = opd
+    @program_kegiatan = program_kegiatan
+  end
+
+  def program_renstra
+    program_opd.indikator_program_renstra
+  end
+
+  def program_opd
+    program_kegiatan_opd.programs
+  end
+
+  def indikator_program
+    @program_kegiatan.indikator_program_renstra
+  end
+
+  def program_kegiatan_opd
+    @opd.program_kegiatans
+  end
+
+  def program_kegiatan
+    ProgramKegiatan.find_by(kode_program: @kode_program)
+  end
+
+  def opd
+    Opd.find_by(kode_unik_opd: @kode_unik_opd)
+  end
+end
