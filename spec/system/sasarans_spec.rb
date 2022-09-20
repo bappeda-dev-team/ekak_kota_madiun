a2c24eb013331e77558c7fe978b2eed27d3b0d0f
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-09-20 15:42:59 +0700

    add more spec sasaran

--- spec/system/sasarans_spec.rb
+++ spec/system/sasarans_spec.rb
@@ -4,24 +4,32 @@ RSpec.describe "CRUD Sasaran", type: :system do
   let(:user) { FactoryBot.create(:asn) }
   let(:sasaran) { FactoryBot.create(:sasaran) }
 
-  scenario 'asn dashboard page' do
-    user
-    visit root_path
-    fill_in "user_login", with: user.email
-    fill_in "password", with: user.password
-    click_button "Sign in"
-    expect(page).to have_content('Dashboard')
-    expect(page).to have_content('Sasaran Kinerja / Rencana Kinerja')
-    expect(page).to have_content('Jumlah sasaran: 0')
-  end
-
-  scenario 'asn with at least one sasaran' do
+  before(:each) do
     user.sasarans.create!(sasaran_kinerja: 'Test Sasaran')
     visit root_path
     fill_in "user_login", with: user.email
     fill_in "password", with: user.password
     click_button "Sign in"
-    expect(page).to have_content('Sasaran Kinerja / Rencana Kinerja')
-    expect(page).to have_content('Jumlah sasaran: 1')
+  end
+
+  context 'first success login' do
+    scenario 'asn with at least one sasaran but not shown' do
+      expect(page).to have_content('Sasaran Kinerja / Rencana Kinerja')
+      expect(page).to have_content('Jumlah sasaran: 1')
+      expect(page).to_not have_content('Test Sasaran')
+    end
+  end
+
+  context 'rencana kinerja asn' do
+    scenario 'asn see their sasaran here' do
+      visit user_sasarans_path(user)
+      expect(page).to have_content('Test Sasaran')
+    end
+    it 'show their sasaran' do
+      sasaran = user.sasarans.first
+      visit user_sasarans_path(user)
+      first(:css, 'i.fas.fa-book-open').click
+      expect(page).to have_current_path(user_sasaran_path(user, sasaran))
+    end
   end
 end
