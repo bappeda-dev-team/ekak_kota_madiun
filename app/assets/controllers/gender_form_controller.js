22cf110c3e15530ee48f02a9ebfb4247ba740405
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-10-13 10:44:04 +0700

    permasalahan dibawah data terpilah

--- app/assets/controllers/gender_form_controller.js
+++ app/assets/controllers/gender_form_controller.js
@@ -1,16 +1,17 @@
-import { Controller } from 'stimulus'
+import {Controller} from 'stimulus'
 
 
 export default class extends Controller {
 
-  static targets = [ 
-                    "sasaran", "penerima_manfaat", "data_terpilah", "penyebab_internal", "penyebab_external",
-                    "indikator_sasaran", "target_indikator", "satuan_indikator", "rencana_aksi",
-                    "program_kegiatan_id", "sasaran_id" ]
+  static targets = [
+    "sasaran", "penerima_manfaat", "data_terpilah", "permasalahan",
+    "penyebab_internal", "penyebab_external",
+    "indikator_sasaran", "target_indikator", "satuan_indikator", "rencana_aksi",
+    "program_kegiatan_id", "sasaran_id"]
 
   connect() {
-    if(this.hasSasaran_idTarget) {
-      if(this.sasaran_idTarget.value != "") {
+    if (this.hasSasaran_idTarget) {
+      if (this.sasaran_idTarget.value != "") {
         this.fill_default_data(this.sasaran_idTarget.value)
       }
     }
@@ -23,10 +24,11 @@ export default class extends Controller {
         this.sasaranTarget.value = data.sasaran_kinerja
         this.penerima_manfaatTarget.value = data.penerima_manfaat
         this.data_terpilahTarget.value = data.data_terpilah
+        this.permasalahanTarget.value = data.permasalahan
       })
     // data rencana aksi
     const data_rencana_aksi = fetch(`/sasarans/${id_sasaran}/rencana_aksi`)
-                              .then((res) => res.text())
+      .then((res) => res.text())
       .then((html) => {
         this.rencana_aksiTarget.innerHTML = html
       })
@@ -47,15 +49,16 @@ export default class extends Controller {
         this.data_terpilahTargets.forEach((data_terpilah) => {
           data_terpilah.value = data.data_terpilah
         })
+        this.permasalahanTarget.value = data.permasalahan
         this.penyebab_internalTarget.value = data.penyebab_internal
         this.penyebab_externalTarget.value = data.penyebab_external
-        this.indikator_sasaranTarget.value = indikator_sasaran[0].indikator 
-        this.target_indikatorTarget.value = indikator_sasaran[0].target 
-        this.satuan_indikatorTarget.value = indikator_sasaran[0].satuan 
+        this.indikator_sasaranTarget.value = indikator_sasaran[0].indikator
+        this.target_indikatorTarget.value = indikator_sasaran[0].target
+        this.satuan_indikatorTarget.value = indikator_sasaran[0].satuan
       })
     // data rencana aksi
     const data_rencana_aksi = fetch(`/sasarans/${id_sasaran}/rencana_aksi`)
-                              .then((res) => res.text())
+      .then((res) => res.text())
       .then((html) => {
         this.rencana_aksiTarget.innerHTML = html
       })
