f36f2a9a7004e11fa2087abbfbda10cfec4fc47a
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-11-18 14:31:27 +0700

    fix daftar resiko kurang satu pdf_rka satuan tidak sesuai dashboard hanya menampilkan tahun terpilih

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def dashboard
-    @tahun_sasaran = cookies[:tahun_sasaran] || '2023'
+    @tahun_sasaran = cookies[:tahun_sasaran]
     @sasarans = current_user.sasarans.where('tahun ILIKE ?',
                                             "%#{@tahun_sasaran}%")
                             .or(current_user.sasarans.where(sasarans: { tahun: nil })).order(:id)
