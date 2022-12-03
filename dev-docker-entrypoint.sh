56d95947b72d53f8fba728a6ff978732e136e214
Ryan B <ryan.brilliant.nirwana@gmail.com>
2022-12-04 06:18:21 +0700

    fix docker and webpacker configuration

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 rm -f /app/tmp/pids/server.pid
 bundle install
 
-bin/rails db:migrate || bin/rails db:setup
+bin/rails db:migrate || bin/rails db:setup && bin/rails db:migrate && bin/rails db:seed
 bin/rails yarn:install
+bin/rails s -b 0.0.0.0
