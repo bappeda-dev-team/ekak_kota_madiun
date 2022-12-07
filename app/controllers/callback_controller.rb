2ce13113b380cf5692ec1fc51f1cba63f84559fe
Ryan B <ryan.brilliant.nirwana@gmail.com>
2022-12-07 16:25:27 +0700

    daredevil

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -4,35 +4,24 @@ class CallbackController < ApplicationController
   URL = 'https://manekin.madiunkota.go.id/oauth/token'.freeze
   URL_USER = 'https://manekin.madiunkota.go.id/api/user'.freeze
   def index
-    begin
-      response = HTTP.accept(:json).post(URL,
-                                         form: { grant_type: 'authorization_code',
-                                                 client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
-                                                 client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
-                                                 redirect_uri: callback_path,
-                                                 code: params[:code] })
-      logger.error "response callback -> #{response.code}"
-      data = Oj.load(response.body)
-      logger.error "result -> #{response}"
-      logger.error "response json body-> #{data}"
-      access_token = data['access_token']
-    rescue StandardError
-      logger.error "error authorization"
-      logger.error "params_code -> #{params[:code]}"
-      logger.error "token -> #{access_token}"
-    end
+    response = HTTP.accept(:json).post(URL,
+                                       form: { grant_type: 'authorization_code',
+                                               client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
+                                               client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
+                                               redirect_uri: callback_path,
+                                               code: params[:code] })
+    logger.error "response callback -> #{response.code}"
+    data = Oj.load(response.body)
+    logger.error "result -> #{response}"
+    logger.error "response json body-> #{data}"
+    access_token = data['access_token']
 
     # api user
-    begin
-      user_response = HTTP.accept(:json).auth("Bearer #{access_token}").get(URL_USER)
-      logger.error "response callback -> #{user_response.code}"
-      user_data = Oj.load(user_response.body)
-      logger.error "response json -> #{user_data}"
-      username = user_data['username']
-    rescue StandardError
-      logger.error "error user"
-      logger.error "username -> #{username}"
-    end
+    user_response = HTTP.accept(:json).auth("Bearer #{access_token}").get(URL_USER)
+    logger.error "response callback -> #{user_response.code}"
+    user_data = Oj.load(user_response.body)
+    logger.error "response json -> #{user_data}"
+    username = user_data['username']
 
     user = User.find_by(nik: username)
     respond_to do |format|
