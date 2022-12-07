5a511e08c58bedf76c03180101a864545707d1df
Ryan B <ryan.brilliant.nirwana@gmail.com>
2022-12-07 18:12:34 +0700

    disable cert verification

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,19 +1,18 @@
 class CallbackController < ApplicationController
-  require 'net/http'
-
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
-
   URL = 'https://manekin.madiunkota.go.id/oauth/token'.freeze
   URL_USER = 'https://manekin.madiunkota.go.id/api/user'.freeze
   def index
-    uri = URI(URL)
-    response = Net::HTTP.post_form(uri,
-                                   grant_type: 'authorization_code',
-                                   client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
-                                   client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
-                                   redirect_uri: callback_path,
-                                   code: params[:code])
+    ctx = OpenSSL::SSL::SSLContext.new
+    ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    response = HTTP.accept(:json).post(URL,
+                                       form: { grant_type: 'authorization_code',
+                                               client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
+                                               client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
+                                               redirect_uri: callback_path,
+                                               code: params[:code] })
     logger.error "response callback -> #{response.code}"
     data = Oj.load(response.body)
     logger.error "result -> #{response}"
@@ -26,7 +25,6 @@ class CallbackController < ApplicationController
     user_data = Oj.load(user_response.body)
     logger.error "response json -> #{user_data}"
     username = user_data['username']
-
     user = User.find_by(nik: username)
     respond_to do |format|
       if user
@@ -36,5 +34,8 @@ class CallbackController < ApplicationController
         format.html { redirect_to root_path, alert: 'NIP Tidak ditemukan' }
       end
     end
+  rescue StandardError
+    logger.error "error user"
+    logger.error "username -> #{username}"
   end
 end
