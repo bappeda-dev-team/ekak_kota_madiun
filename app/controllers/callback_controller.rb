09f1eeb8f119cc85eeecb7f0ccb31469dcb87ec6
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2022-12-07 16:02:22 +0700

    add more logger

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -11,17 +11,23 @@ class CallbackController < ApplicationController
                                                  client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
                                                  redirect_uri: 'https://kak.madiunkota.go.id/callback',
                                                  code: params[:code] })
+      logger.error "response callback -> #{response.code}"
       data = Oj.load(response.body)
+      logger.error "result -> #{response}"
+      logger.warn "response json body-> #{data}"
       access_token = data['access_token']
     rescue StandardError
       logger.error "error authorization"
       logger.error "params_code -> #{params[:code]}"
+      logger.error "token -> #{access_token}"
     end
 
     # api user
     begin
       user_response = HTTP.accept(:json).auth("Bearer #{access_token}").get(URL_USER)
+      logger.error "response callback -> #{user_response.code}"
       user_data = Oj.load(user_response.body)
+      logger.warn "response json -> #{user_data}"
       username = user_data['username']
     rescue StandardError
       logger.error "error user"
