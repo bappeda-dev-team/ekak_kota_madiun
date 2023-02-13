08d7ab219e047dd0499d3bb04d0c04b21f17f5fc
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-02-14 00:44:49 +0700

    tujuan kota tweaked

--- app/controllers/tujuan_kota_controller.rb
+++ app/controllers/tujuan_kota_controller.rb
@@ -1,5 +1,65 @@
 class TujuanKotaController < ApplicationController
+  before_action :set_tujuan_kotau, only: %i[show edit update destroy]
+
   def index
-    @tujuan_kota = TujuanKota.all
+    handle_filters
+  end
+
+  def show; end
+
+  def new
+    @tujuan_kota = TujuanKota.new
+  end
+
+  def edit; end
+
+  def create
+    @tujuan_kota = TujuanKota.new(tujuan_kota_params)
+
+    respond_to do |format|
+      if @tujuan_kota.save
+        format.html { redirect_to tujuan_kota_path, success: 'Tujuan ditambahkan' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @tujuan_kota.update(tujuan_kota_params)
+        format.html { redirect_to tujuan_kota_path, success: 'Tujuan diupdate' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @tujuan_kota.destroy
+    respond_to do |format|
+      format.html { redirect_to tujuan_kota_path, success: 'Tujuan dihapus' }
+    end
+  end
+
+  private
+
+  def set_tujuan_kota
+    @tujuan_kota = TujuanKota.find(params[:id])
+  end
+
+  def tujuan_kota_params
+    params.require(:tujuan_kota).permit(:tujuan, :tahun_awal, :tahun_akhir)
+  end
+
+  def handle_filters
+    tahun = params[:tahun]
+    if tahun.nil? || tahun == 'all'
+      @tahun = ''
+      @tujuan_kota = TujuanKota.all.includes([:indikator_tujuans])
+    else
+      @tujuan_kota = TujuanKota.where(tahun_awal: tahun).or(TujuanKota.where(tahun_akhir: tahun)).includes([:indikator_tujuans])
+      @tahun = "Tahun #{tahun}"
+    end
   end
 end
