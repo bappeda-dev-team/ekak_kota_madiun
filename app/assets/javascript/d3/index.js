347cf2cde38d8dfd26d51a48e64b890c0216c32e
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-02-20 06:45:34 +0700

    base api untuk pokin kota

--- app/assets/javascript/d3/index.js
+++ app/assets/javascript/d3/index.js
@@ -1,66 +1,31 @@
 import 'd3-mitch-tree/dist/css/d3-mitch-tree-theme-default.min.css'
 const mitchTree = require('d3-mitch-tree')
 
-// dummy data
-var data = {
-        "id": 1,
-        "name": "Pohon Kinerja",
-        "type": "Root",
-        "description": "Kota Madiun",
-        "children": [{
-                "id": 2,
-                "name": "Isu Strategis",
-                "type": "isu_strategis_kota",
-                "description": "Masih Terbatasnya Kualitas Sumber Daya Manusia",
-                "children": [
-                        {
-                                "id": 3,
-                                "name": "Strategi Kota",
-                                "type": "strategi_kota",
-                                "description": "Pemerataan Kualitas Sekolah",
-                                "children": [
-                                        {
-                                                "id": 4,
-                                                "name": "Perangkat Daerah",
-                                                "type": "opd",
-                                                "description": "Dinas Pendidikan",
-                                        },
-
-                                        {
-                                                "id": 5,
-                                                "name": "Perangkat Daerah",
-                                                "type": "opd",
-                                                "description": "Badan Perencanaan",
-                                        },
-                                        {
-                                                "id": 6,
-                                                "name": "Perangkat Daerah",
-                                                "type": "opd",
-                                                "description": "Dinas Pekerjaan Umum",
-                                        },
-                                ]
-                        }
-                ]
-        }
-        ]
-};
-
 function init() {
-        const treePlugin = new mitchTree.boxedTree()
-                .setData(data)
-                .setElement(document.getElementById('pokin'))
-                .setIdAccessor(function(data) {
-                        return data.id;
-                })
-                .setChildrenAccessor(function(data) {
-                        return data.children;
-                })
-                .setBodyDisplayTextAccessor(function(data) {
-                        return data.description;
-                })
-                .setTitleDisplayTextAccessor(function(data) {
-                        return data.name;
-                })
-                .initialize();
+        const tahun = 2023
+        const targetEl = document.getElementById('pokin')
+        if (targetEl) {
+                fetch(`/isu_strategis_kota.json?tahun=${tahun}`)
+                        .then(response => response.json())
+                        .then((data) => {
+                                const isu_kota = data['results']
+                                new mitchTree.boxedTree()
+                                        .setData(isu_kota)
+                                        .setElement(targetEl)
+                                        .setIdAccessor(function(data) {
+                                                return data.id;
+                                        })
+                                        .setChildrenAccessor(function(data) {
+                                                return data.children;
+                                        })
+                                        .setBodyDisplayTextAccessor(function(data) {
+                                                return data.description;
+                                        })
+                                        .setTitleDisplayTextAccessor(function(data) {
+                                                return data.name;
+                                        })
+                                        .initialize();
+                        })
+        }
 }
 window.addEventListener('DOMContentLoaded', init);
---
+++ app/views/isu_strategis_kota/index.json.jbuilder
@@ -0,0 +1,34 @@
+json.results do
+  json.id 0
+  json.name "Pohon Kinerja"
+  json.type "root"
+  json.description "Kota Madiun"
+  json.tahun @tahun
+  if @isu_strategis_kota.any?
+    json.children @isu_strategis_kota do |isu_kota|
+      json.id isu_kota.id
+      json.name "Isu Strategis"
+      json.type "isu_strategis_kota"
+      json.description isu_kota.isu_strategis
+      json.children isu_kota.strategi_kotums do |strategi_kota|
+        json.id strategi_kota.id
+        json.name "Strategi"
+        json.type "strategi_kota"
+        json.description strategi_kota.strategi
+        json.children strategi_kota.usulans do |opd|
+          json.id opd.id
+          json.name "Perangkat Daerah"
+          json.type "perangkat_daerah"
+          json.description opd.keterangan
+        end
+      end
+    end
+  else
+    json.children do
+      json.id 1
+      json.name "Isu Strategis"
+      json.type "isu_strategis_kota"
+      json.description "Isu Strategis Kota Kosong"
+    end
+  end
+end
