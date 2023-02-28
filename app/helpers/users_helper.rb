4c3ace4d961aa5d6565691b4ad918788f897fc4d
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2023-02-28 16:57:58 +0700

    add more safer guard and moved to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,13 @@ module UsersHelper
   def nama_asn(nik:)
     User.find_by(nik: nik)&.nama
   end
+
+  def dropdown_opd_user(user)
+    if current_user.has_role? :super_admin
+      options_for_select(Opd.where.not(kode_opd: nil).pluck(:nama_opd, :kode_opd), user.opd.kode_opd)
+    else
+      options_for_select(Opd.where.not(kode_opd: nil).where(kode_opd: user.opd.kode_opd).pluck(:nama_opd, :kode_opd),
+                         user.opd.kode_opd)
+    end
+  end
 end
