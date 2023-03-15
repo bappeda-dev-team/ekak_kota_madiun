9d221c55f1443c2609f9f512a16270e0a8c24d3c
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-03-15 13:02:31 +0700

    danger tahapan urutan yang benar

--- app/models/tahapan.rb
+++ app/models/tahapan.rb
@@ -31,7 +31,7 @@ class Tahapan < ApplicationRecord
 
   validates :tahapan_kerja, presence: true
 
-  default_scope { order(urutan: :asc) }
+  default_scope { order('urutan::integer ASC') }
 
   amoeba do
     append id_rencana: '_2022_p'
