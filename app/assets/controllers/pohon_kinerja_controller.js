9af08dfedc2142981999142c8a854aa20e00bca7
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-03-16 03:15:33 +0700

    pokin refactoring

--- app/assets/controllers/pohon_kinerja_controller.js
+++ app/assets/controllers/pohon_kinerja_controller.js
@@ -11,64 +11,96 @@ import { Controller } from "stimulus"
 
 import 'd3-mitch-tree/dist/css/d3-mitch-tree-theme-default.min.css'
 export default class extends Controller {
-        static targets = ["pokin"]
+        static targets = ["pokin", "buttonToggle"]
 
         static values = {
                 opd: Number,
-                tahun: Number
+                tahun: Number,
+                render: Boolean
         }
+
+        togglePokin() {
+                const isShow = !this.renderValue
+                this.showPokin(isShow)
+                this.renderValue = isShow
+        }
+
+        showPokin(isShow) {
+                if (isShow) {
+                        this.pokinTarget.style.display = 'block'
+                        this.pokinRender()
+                        this.buttonToggleTarget.classList.remove('btn-outline-success')
+                        this.buttonToggleTarget.classList.add('btn-outline-danger')
+                        this.buttonToggleTarget.innerText = 'Sembunyikan Pohon'
+                }
+                else {
+                        this.pokinTarget.style.display = 'none'
+                        this.buttonToggleTarget.classList.remove('btn-outline-danger')
+                        this.buttonToggleTarget.classList.add('btn-outline-success')
+                        this.buttonToggleTarget.innerText = 'Tampilkan Pohon'
+                }
+        }
+
         pokinTargetConnected() {
+                this.pokinTarget.style.display = 'none'
+        }
+
+        mithTreeRender(data, targetEl) {
                 const mitchTree = require('d3-mitch-tree')
+                const isu_kota = data['results']
+                const treePlugin = new mitchTree.boxedTree()
+                        .setData(isu_kota)
+                        .setElement(targetEl)
+                        .setIdAccessor(function(data) {
+                                return data.id;
+                        })
+                        .setChildrenAccessor(function(data) {
+                                return data.children;
+                        })
+                        .setBodyDisplayTextAccessor(function(data) {
+                                return data.description;
+                        })
+                        .setTitleDisplayTextAccessor(function(data) {
+                                return data.name;
+                        })
+                        .setOrientation('topToBottom')
+                        .setAllowNodeCentering(true)
+                        .setNodeDepthMultiplier(250)
+                        .setWidthWithoutMargins(550)
+                        .setHeightWithoutMargins(550)
+                        .getNodeSettings()
+                        .setSizingMode('nodeSize')
+                        .setHorizontalSpacing(40)
+                        .setVerticalSpacing(40)
+                        .setBodyBoxWidth(350)
+                        .setBodyBoxHeight(120)
+                        .setBodyBoxPadding({
+                                top: 5,
+                                bottom: 5,
+                                right: 7,
+                                left: 7
+                        })
+                        .back()
+                        .initialize();
+                return treePlugin;
+        }
+
+        pokinRender() {
 
                 const targetEl = this.pokinTarget
                 const url = `/opds/kotak_usulan.json?kode_opd=${this.opdValue}&tahun=${this.tahunValue}`
-                if (targetEl) {
-                        fetch(url)
-                                .then(response => response.json())
-                                .then((data) => {
-                                        const isu_kota = data['results']
-                                        const treePlugin = new mitchTree.boxedTree()
-                                                .setData(isu_kota)
-                                                .setElement(targetEl)
-                                                .setIdAccessor(function(data) {
-                                                        return data.id;
-                                                })
-                                                .setChildrenAccessor(function(data) {
-                                                        return data.children;
-                                                })
-                                                .setBodyDisplayTextAccessor(function(data) {
-                                                        return data.description;
-                                                })
-                                                .setTitleDisplayTextAccessor(function(data) {
-                                                        return data.name;
-                                                })
-                                                .setOrientation('topToBottom')
-                                                .setAllowNodeCentering(true)
-                                                .setNodeDepthMultiplier(250)
-                                                .setWidthWithoutMargins(550)
-                                                .setHeightWithoutMargins(550)
-                                                .getNodeSettings()
-                                                .setSizingMode('nodeSize')
-                                                .setHorizontalSpacing(40)
-                                                .setVerticalSpacing(40)
-                                                .setBodyBoxWidth(350)
-                                                .setBodyBoxHeight(120)
-                                                .setBodyBoxPadding({
-                                                        top: 5,
-                                                        bottom: 5,
-                                                        right: 7,
-                                                        left: 7
-                                                })
-                                                .back()
-                                                .initialize();
-                                        // auto expand
-                                        var nodes = treePlugin.getNodes();
-                                        nodes.forEach(function(node, index, arr) {
-                                                treePlugin.expand(node);
-                                        });
-                                        treePlugin.update(treePlugin.getRoot());
-                                })
-                }
-                // window.addEventListener('DOMContentLoaded', init);
+                fetch(url)
+                        .then(response => response.json())
+                        .then((data) => {
+                                const treePlugin = this.mithTreeRender(data, targetEl)
+                        })
+        }
+
+        expandAllTree(treePlugin) {
+                var nodes = treePlugin.getNodes();
+                nodes.forEach(function(node, index, arr) {
+                        treePlugin.expand(node);
+                });
+                treePlugin.update(treePlugin.getRoot());
         }
 }
