5a4964cdffad89b1018c48ab22401a9b04a419a6
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-05-22 03:15:52 +0700

    anggaran lain lain dan tombol gaji

---
+++ app/helpers/rincian_belanja_helper.rb
@@ -0,0 +1,31 @@
+module RincianBelanjaHelper
+  def link_edit_anggaran(anggaran, hitung)
+    if hitung.deskripsi.downcase.include?('lain_lain')
+      edit_lain_lain_anggaran_perhitungan_path(anggaran, hitung)
+    elsif hitung.spesifikasi.include?('Belanja Gaji')
+      edit_gaji_anggaran_perhitungan_path(anggaran, hitung)
+    else
+      edit_anggaran_perhitungan_path(anggaran, hitung)
+    end
+  end
+
+  def uraian_rincian_anggaran(hitung)
+    if hitung.deskripsi.downcase.include?('lain_lain') || hitung.spesifikasi.include?('Belanja Gaji')
+      uraian = hitung.deskripsi
+      spesifikasi = hitung.spesifikasi
+      tahun = hitung.tahun
+    else
+      uraian = uraian_kode(hitung.deskripsi)
+      spesifikasi = spesifikasi_anggaran(hitung.deskripsi)
+      tahun = tahun_kode(hitung.deskripsi)
+    end
+    "#{uraian} <br/> #{spesifikasi} <br/> - #{tahun}".html_safe
+  end
+
+  def spesifikasi_anggaran(kode_barang)
+    # update using delgate method polymorphic
+    Search::AllAnggaran.find_by_kode_barang(kode_barang).spesifikasi
+  rescue NoMethodError
+    'Tidak Ditemukan'
+  end
+end
