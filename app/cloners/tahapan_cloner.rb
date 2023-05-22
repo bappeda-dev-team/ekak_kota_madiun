1d8403e1765b1f185abed65abe8d059de9f9b3c6
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-05-22 12:35:45 +0700

    bug clone tahapna

--- app/cloners/tahapan_cloner.rb
+++ app/cloners/tahapan_cloner.rb
@@ -6,8 +6,11 @@ class TahapanCloner < Clowne::Cloner
     nullify :jumlah_target
   end
 
-  finalize do |source, record, id_rencana_sas:, **|
+  finalize do |_source, record, id_rencana_sas:, **|
     record.id_rencana = id_rencana_sas
+    record.id_rencana_aksi = SecureRandom.base36(6)
+    record.target = 0
+    record.jumlah_target = 0
     record.created_at = Time.current
     record.updated_at = Time.current
   end
