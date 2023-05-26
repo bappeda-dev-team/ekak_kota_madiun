7d780eec6378b647e02314c229f74bf92b24e1f5
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-05-26 10:05:00 +0700

    programkegiatan puskesmas

--- app/controllers/program_kegiatans_controller.rb
+++ app/controllers/program_kegiatans_controller.rb
@@ -14,13 +14,16 @@ class ProgramKegiatansController < ApplicationController
     # FIXME: REFACTOR TOO MUCH LOGIC
     @program_kegiatans = ProgramKegiatan.where("kode_opd ILIKE ?", "%#{current_user.kode_opd}%")
                                         .where("nama_subkegiatan ILIKE ?", "%#{param}%")
-                                        .uniq(&:kode_sub_giat)
     if current_user.pegawai_kelurahan?
-      @program_kegiatans = @programKegiatans.select do |program|
+      @program_kegiatans = @program_kegiatans.select do |program|
         program.nama_opd_pemilik.upcase.split(/KELURAHAN/, 2).last.strip == current_user.petunjuk_kelurahan
       end
+    elsif current_user.pegawai_puskesmas?
+      @program_kegiatans = @program_kegiatans.select do |program|
+        program.nama_opd_pemilik.upcase.split(/PUSKESMAS/, 2).last.strip == current_user.petunjuk_puskesmas
+      end
     elsif current_user.pegawai_bagian?
-      @program_kegiatans = @programKegiatans.select do |program|
+      @program_kegiatans = @program_kegiatans.select do |program|
         program.nama_opd_pemilik.upcase.split(/BAGIAN/, 2).last.strip == current_user.petunjuk_bagian
       end
     end
