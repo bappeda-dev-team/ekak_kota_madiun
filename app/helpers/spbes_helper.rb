879434917a07429abcf3248d8335110d24c9a47b
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-06-06 05:38:25 +0700

    fix rowspan spbes

--- app/helpers/spbes_helper.rb
+++ app/helpers/spbes_helper.rb
@@ -1,2 +1,8 @@
 module SpbesHelper
+  def rowspan_spbe(spbes)
+    sasaran_size = spbes.inject(0) do |sum, spbe|
+      sum + spbe.sasaran.operational_objectives.length
+    end
+    spbes.size + sasaran_size + 1
+  end
 end
