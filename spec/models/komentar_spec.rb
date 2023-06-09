68caf1e89ef52ac9e4bc1f3b6b45469065d0c070
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-06-09 09:06:39 +0700

    fitur komentar di pokin , minus rekap opd

---
+++ spec/models/komentar_spec.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: komentars
+#
+#  id         :bigint           not null, primary key
+#  item       :bigint
+#  judul      :string
+#  kode_opd   :string
+#  komentar   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_komentars_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+require 'rails_helper'
+
+RSpec.describe Komentar, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
