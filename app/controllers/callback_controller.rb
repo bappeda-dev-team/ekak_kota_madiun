6562e0167e3f4ff7fb746f6dc1a3a878a2449193
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-07-29 00:05:28 +0700

    fix thing sso

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -3,36 +3,39 @@ class CallbackController < ApplicationController
   skip_before_action :authenticate_user!
   URL = 'https://manekin.madiunkota.go.id/oauth/token'.freeze
   URL_USER = 'https://manekin.madiunkota.go.id/api/user'.freeze
+
   def index
     ctx = OpenSSL::SSL::SSLContext.new
     ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = HTTP.accept(:json).post(URL,
-                                       json: {
-                                         'form-data' => {
-                                           grant_type: 'authorization_code',
-                                           client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
-                                           client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
-                                           redirect_uri: callback_path,
-                                           code: params[:code],
-                                           scope: '*'
-                                         }
-                                       },
-                                       ssl_context: ctx)
-    data = Oj.load(response.body)
+    code = params[:code]
+    param_sso = {
+      grant_type: 'authorization_code',
+      client_id: '97dd802d-9840-4f0b-98c1-96fb80dc7b92',
+      client_secret: 'X2a71ep0QzpuvEBjjvTQzTv7A7J7Z7CWpunZbTJw',
+      redirect_uri: callback_path,
+      code: code,
+      scope: '*'
+    }
+    logger = Logger.new(STDOUT)
+    http = HTTP.use(logging: { logger: logger })
+
+    response = http.post(URL, form: param_sso, ssl_context: ctx)
+
+    data = JSON.parse response.body.to_s
     access_token = data['access_token']
     user_login(access_token)
   rescue StandardError
     redirect_to root_path, alert: 'Terjadi Kesalahan'
   end
 
-  private
-
   def user_login(access_token)
     ctx = OpenSSL::SSL::SSLContext.new
     ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    logger = Logger.new(STDOUT)
+    http = HTTP.use(logging: { logger: logger })
     # api user
-    user_response = HTTP.accept(:json).auth("Bearer #{access_token}").get(URL_USER, ssl_context: ctx)
-    user_data = Oj.load(user_response.body)
+    response = http.auth("Bearer #{access_token}").get(URL_USER, form: {}, ssl_context: ctx)
+    user_data = JSON.parse response.body.to_s
     username = user_data['username']
     user = User.find_by(nik: username)
     sign_in(user)
