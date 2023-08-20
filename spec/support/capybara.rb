62ab7c93584e3d0ee0f139f7f147ce40dcb67ee1
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-08-21 00:55:52 +0700

    modify capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,19 +1,18 @@
 require "capybara/rspec"
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false
+  # config.use_transactional_fixtures = false
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.before(:each) { DatabaseCleaner.strategy = :transaction }
+  # config.before(:each) { DatabaseCleaner.strategy = :transaction }
 
   config.before(:each, type: :feature) do
     # :rack_test driver's Rack app under test shares database connection
     # with the specs, so continue to use transaction strategy for speed.
-    driver_shares_db_connection_with_specs =
-      Capybara.current_driver == :rack_test
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
 
     unless driver_shares_db_connection_with_specs
       # Driver is probably for an external browser with an app
@@ -23,7 +22,11 @@ RSpec.configure do |config|
     end
   end
 
-  config.before(:each) { DatabaseCleaner.start }
+  config.before(:each, type: :feature, js: true) do
+    Capybara.current_driver = :selenium
+  end
 
-  config.append_after(:each) { DatabaseCleaner.clean }
+  # config.before(:each) { DatabaseCleaner.start }
+
+  # config.append_after(:each) { DatabaseCleaner.clean }
 end
