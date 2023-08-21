3f2c3b426a07ab8768cad8d6f665606da3842f14
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-08-22 05:38:10 +0700

    tematik accept indikators attributes

--- app/controllers/tematiks_controller.rb
+++ app/controllers/tematiks_controller.rb
@@ -140,7 +140,7 @@ class TematiksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def tematik_params
-    params.require(:tematik).permit(:tema, :keterangan)
+    params.require(:tematik).permit(:tema, :keterangan, indikators_attributes)
   end
 
   def sub_tematik_params
