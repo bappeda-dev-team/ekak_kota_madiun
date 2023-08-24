e26cc269894ce098b2f39e39e2fc2f64ede0f877
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-08-24 10:12:04 +0700

    pohon_presenter in pohon kinerja

---
+++ app/presenters/pohon_kinerja_presenter.rb
@@ -0,0 +1,25 @@
+class PohonKinerjaPresenter
+  attr_accessor :pohon
+
+  delegate :id, :to_param,
+           to: :pohon
+  def initialize(pohon)
+    @pohon = pohon
+  end
+
+  def pohonable
+    @pohon.instance_of?(Pohon) ? @pohon.pohonable : @pohon
+  end
+
+  def keterangan
+    @pohon.instance_of?(Pohon) ? @pohon.keterangan : ''
+  end
+
+  def tombol_partial
+    @pohon.instance_of?(Pohon) ? 'pohon_kinerja/item_pohon_tombol' : 'pohon_kinerja/item_pohon_tombol_opd'
+  end
+
+  def role
+    @pohon.role
+  end
+end
