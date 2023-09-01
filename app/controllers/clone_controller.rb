a3cce9e58d272039e468bf7d410ed05a5a172949
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-09-02 01:57:25 +0700

    clone controller spec for pohon_tematik

--- app/controllers/clone_controller.rb
+++ app/controllers/clone_controller.rb
@@ -12,90 +12,98 @@ class CloneController < ApplicationController
 
     pohon = Pohon.find(params[:id])
 
-    ket = "clone_#{tahun_asal}_#{@tahun}"
+    ket = "clone_dari_#{tahun_asal}"
 
-    child_level1 = Pohon.where(pohon_ref_id: pohon.id)
-    child_level2 = Pohon.where(pohon_ref_id: child_level1.map(&:id))
-    child_level3 = Pohon.where(pohon_ref_id: child_level2.map(&:id))
-    child_level4 = Pohon.where(pohon_ref_id: child_level3.map(&:id))
-    child_level5 = Pohon.where(pohon_ref_id: child_level4.map(&:id))
-    child_level6 = Pohon.where(pohon_ref_id: child_level5.map(&:id))
+    # child_level1 = Pohon.where(pohon_ref_id: pohon.id)
+    # child_level2 = Pohon.where(pohon_ref_id: child_level1.map(&:id))
+    # child_level3 = Pohon.where(pohon_ref_id: child_level2.map(&:id))
+    # child_level4 = Pohon.where(pohon_ref_id: child_level3.map(&:id))
+    # child_level5 = Pohon.where(pohon_ref_id: child_level4.map(&:id))
+    # child_level6 = Pohon.where(pohon_ref_id: child_level5.map(&:id))
 
-    operation = PohonCloner.call(pohon, tahun: @tahun, pohon_ref_id: '', keterangan: ket)
+    operation = PohonCloner.call(pohon, traits: :pohon_tematik,
+                                        tahun: @tahun, pohon_ref_id: '', keterangan: ket)
     operation.to_record
     operation.persist!
     clone_pohon = operation.to_record
-    clone_pohon_id = clone_pohon.id
+    # clone_pohon_id = clone_pohon.id
 
-    clone_level1 = []
-    child_level1.each do |lvl1|
-      op = PohonCloner.call(lvl1, tahun: @tahun, pohon_ref_id: clone_pohon_id, keterangan: ket)
-      op.to_record
-      op.persist!
-      clone = op.to_record
-      clone_level1.append(clone.id)
-    end
+    # clone_level1 = []
+    # child_level1.each do |lvl1|
+    #   op = PohonCloner.call(lvl1, traits: :pohon_tematik,
+    #                               tahun: @tahun, pohon_ref_id: clone_pohon_id, keterangan: ket)
+    #   op.to_record
+    #   op.persist!
+    #   clone = op.to_record
+    #   clone_level1.append(clone.id)
+    # end
 
-    return if clone_level1.empty?
+    # return if clone_level1.empty?
 
-    clone_level2 = []
-    child_level2.each do |lvl2|
-      clone_level1.each do |lvl1|
-        op = PohonCloner.call(lvl2, tahun: @tahun, pohon_ref_id: lvl1, keterangan: ket)
-        op.to_record
-        op.persist!
-        clone = op.to_record
-        clone_level2.append(clone.id)
-      end
-    end
+    # clone_level2 = []
+    # child_level2.each do |lvl2|
+    #   binding.pry
+    #   clone_level1.select { |lv| lv == lvl2.id }.each do |lvl1|
+    #     op = PohonCloner.call(lvl2, traits: :pohon_tematik,
+    #                                 tahun: @tahun, pohon_ref_id: lvl1, keterangan: ket)
+    #     op.to_record
+    #     op.persist!
+    #     clone = op.to_record
+    #     clone_level2.append(clone.id)
+    #   end
+    # end
 
-    return if clone_level2.empty?
+    # return if clone_level2.empty?
 
-    clone_level3 = []
-    child_level3.each do |lvl3|
-      clone_level2.each do |lvl2|
-        op = PohonCloner.call(lvl3, tahun: @tahun, pohon_ref_id: lvl2, keterangan: ket)
-        op.to_record
-        op.persist!
-        clone = op.to_record
-        clone_level3.append(clone.id)
-      end
-    end
+    # clone_level3 = []
+    # child_level3.each do |lvl3|
+    #   clone_level2.select { |lv| lv == lvl3.id }.each do |lvl2|
+    #     op = PohonCloner.call(lvl3, traits: :pohon_tematik,
+    #                                 tahun: @tahun, pohon_ref_id: lvl2, keterangan: ket)
+    #     op.to_record
+    #     op.persist!
+    #     clone = op.to_record
+    #     clone_level3.append(clone.id)
+    #   end
+    # end
 
-    return if clone_level3.empty?
+    # return if clone_level3.empty?
 
-    clone_level4 = []
-    child_level4.each do |lvl4|
-      clone_level3.each do |lvl3|
-        op = PohonCloner.call(lvl4, tahun: @tahun, pohon_ref_id: lvl3, keterangan: ket)
-        op.to_record
-        op.persist!
-        clone = op.to_record
-        clone_level4.append(clone.id)
-      end
-    end
+    # clone_level4 = []
+    # child_level4.each do |lvl4|
+    #   clone_level3.select { |lv| lv == lvl4.id }.each do |lvl3|
+    #     op = PohonCloner.call(lvl4, traits: :pohon_tematik,
+    #                                 tahun: @tahun, pohon_ref_id: lvl3, keterangan: ket)
+    #     op.to_record
+    #     op.persist!
+    #     clone = op.to_record
+    #     clone_level4.append(clone.id)
+    #   end
+    # end
 
-    return if clone_level4.empty?
+    # return if clone_level4.empty?
 
-    clone_level5 = []
-    child_level5.each do |lvl5|
-      clone_level4.each do |lvl4|
-        op = PohonCloner.call(lvl5, tahun: @tahun, pohon_ref_id: lvl4, keterangan: ket)
-        op.to_record
-        op.persist!
-        clone = op.to_record
-        clone_level5.append(clone.id)
-      end
-    end
+    # clone_level5 = []
+    # child_level5.each do |lvl5|
+    #   clone_level4.select { |lv| lv == lvl5.id }.each do |lvl4|
+    #     op = PohonCloner.call(lvl5, traits: :pohon_tematik,
+    #                                 tahun: @tahun, pohon_ref_id: lvl4, keterangan: ket)
+    #     op.to_record
+    #     op.persist!
+    #     clone = op.to_record
+    #     clone_level5.append(clone.id)
+    #   end
+    # end
 
-    return if clone_level5.empty?
+    # return if clone_level5.empty?
 
-    child_level6.each do |lvl6|
-      clone_level5.each do |lvl5|
-        op = PohonCloner.call(lvl6, tahun: @tahun, pohon_ref_id: lvl5)
-        op.to_record
-        op.persist!
-      end
-    end
+    # child_level6.each do |lvl6|
+    #   clone_level5.select { |lv| lv == lvl6.id }.each do |lvl5|
+    #     op = PohonCloner.call(lvl6, traits: :pohon_tematik,
+    #                                 tahun: @tahun, pohon_ref_id: lvl5)
+    #     op.to_record
+    #     op.persist!
+    #   end
+    # end
   end
 end
--- spec/requests/clone_spec.rb
+++ spec/requests/clone_spec.rb
@@ -1,7 +1,123 @@
 require 'rails_helper'
 
 RSpec.describe "Clones", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:tematik) do
+    create :tematik,
+           tema: 'contoh tema a',
+           keterangan: 'contoh keterangan'
+  end
+  let(:sub_tematik) do
+    create :tematik,
+           tema: 'contoh subtema a',
+           type: 'SubTematik',
+           tematik_ref_id: tematik.id,
+           keterangan: 'contoh sub keterangan'
+  end
+  let(:strategic) do
+    create :strategi,
+           strategi: 'strategic 1',
+           type: 'StrategiPohon',
+           tahun: '2023'
+  end
+  let(:tactical) do
+    create :strategi,
+           strategi: 'tactical 1',
+           type: 'StrategiPohon',
+           tahun: '2023'
+  end
+  let(:tactical2) do
+    create :strategi,
+           strategi: 'tactical 2',
+           type: 'StrategiPohon',
+           tahun: '2023'
+  end
+  let(:operational) do
+    create :strategi,
+           strategi: 'operational 1',
+           type: 'StrategiPohon',
+           tahun: '2023'
+  end
+
+  let(:pohon) do
+    create :pohon,
+           pohonable_type: 'Tematik',
+           pohonable_id: tematik.id,
+           keterangan: 'pohon test',
+           pohon_ref_id: '',
+           tahun: '2023'
+  end
+  let(:pohon_child1) do
+    create :pohon,
+           pohonable_type: 'SubTematik',
+           pohonable_id: sub_tematik.id,
+           keterangan: 'pohon test 1',
+           pohon_ref_id: pohon.id,
+           tahun: '2023'
+  end
+  let(:pohon_child2) do
+    create :pohon,
+           pohonable_type: 'Strategi',
+           pohonable_id: strategic.id,
+           keterangan: 'pohon test 2',
+           pohon_ref_id: pohon_child1.id,
+           tahun: '2023'
+  end
+  let(:pohon_child3) do
+    create :pohon,
+           pohonable_type: 'Strategi',
+           pohonable_id: tactical.id,
+           keterangan: 'pohon test 3',
+           pohon_ref_id: pohon_child2.id,
+           tahun: '2023'
+  end
+  let(:pohon_child31) do
+    create :pohon,
+           pohonable_type: 'Strategi',
+           pohonable_id: tactical2.id,
+           keterangan: 'pohon test 3.1',
+           pohon_ref_id: pohon_child2.id,
+           tahun: '2023'
+  end
+  let(:pohon_child4) do
+    create :pohon,
+           pohonable_type: 'Strategi',
+           pohonable_id: operational.id,
+           keterangan: 'pohon test 4',
+           pohon_ref_id: pohon_child3.id,
+           tahun: '2023'
+  end
+  let(:kelompok_anggaran) do
+    create :kelompok_anggaran,
+           tahun: '2023',
+           kelompok: 'Perubahan'
+  end
+
+  describe "pohon_tematik" do
+    before(:each) do
+      sign_in user
+    end
+    it 'should duplicate pohon count' do
+      tahun_asal = '2024'
+      tahun_tujuan = kelompok_anggaran.id
+      pohon_asli = pohon
+      pohon_child1
+      pohon_child2
+      pohon_child3
+      pohon_child31
+      pohon_child4
+
+      jumlah_awal = Pohon.count
+
+      post "/clone/#{pohon_asli.id}/pohon_tematik",
+           params: {
+             tahun_asal: tahun_asal,
+             tahun_tujuan: tahun_tujuan
+           }
+      jumlah_setelah_clone = jumlah_awal * 2
+      pohon_count = Pohon.count
+
+      expect(pohon_count).to eq(jumlah_setelah_clone)
+    end
   end
 end
