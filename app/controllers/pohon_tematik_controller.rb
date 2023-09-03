163784f4dd4b287213768d75c5c4aeacfa41c3ff
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-09-03 10:57:45 +0700

    base pindah opd spec

--- app/controllers/pohon_tematik_controller.rb
+++ app/controllers/pohon_tematik_controller.rb
@@ -72,61 +72,6 @@ class PohonTematikController < ApplicationController
     end
   end
 
-  def new_opd_tematik
-    parent_pohon = Pohon.find(params[:id])
-    @sub_tematik = parent_pohon.pohonable
-    @opds = Opd.opd_resmi.collect { |opd| [opd.nama_opd, opd.id] }
-    @pohon = Pohon.new(pohonable_type: 'Opd', role: 'opd_pohon_kota', tahun: parent_pohon.tahun,
-                       pohon_ref_id: parent_pohon.id)
-  end
-
-  def edit_opd_tematik
-    @pohon = Pohon.find(params[:id])
-    @sub_tematik = Pohon.find(@pohon.pohon_ref_id).pohonable
-    @opds = Opd.opd_resmi.collect { |opd| [opd.nama_opd, opd.id] }
-  end
-
-  def pindah_opd_tematik
-    @pohon = Pohon.find(params[:id])
-    @tahun = @pohon.tahun
-    @dahan_atas = Pohon.where(pohonable_type: 'SubTematik', role: 'sub_pohon_kota', tahun: @tahun)
-                       .collect { |dahan| [dahan.pohonable.tema, dahan.id] }
-  end
-
-  def create_opd_tematik
-    @pohon = Pohon.new(pohon_sub_tema_params)
-    if @pohon.save
-      html_content = render_to_string(partial: 'pohon_tematik/pohon_opd_tematik',
-                                      formats: 'html',
-                                      layout: false,
-                                      locals: { pohon: @pohon })
-      render json: { resText: "OPD Tematik Ditambahkan", attachmentPartial: html_content }.to_json, status: :created
-    else
-      error_content = render_to_string(partial: 'pohon_tematik/form_opd_tematik',
-                                       formats: 'html',
-                                       layout: false,
-                                       locals: { pohon: @pohon })
-      render json: { resText: "Gagal Menyimpan", errors: error_content }.to_json, status: :unprocessable_entity
-    end
-  end
-
-  def update_opd_tematik
-    @pohon = Pohon.find(params[:id])
-    if @pohon.update(pohon_sub_tema_params)
-      html_content = render_to_string(partial: 'pohon_tematik/item_pohon_opd',
-                                      formats: 'html',
-                                      layout: false,
-                                      locals: { pohon: @pohon })
-      render json: { resText: "OPD Tematik Diupdate", attachmentPartial: html_content }.to_json, status: :ok
-    else
-      error_content = render_to_string(partial: 'pohon_tematik/form_opd_tematik',
-                                       formats: 'html',
-                                       layout: false,
-                                       locals: { pohon: @pohon })
-      render json: { resText: "Gagal Menyimpan", errors: error_content }.to_json, status: :unprocessable_entity
-    end
-  end
-
   def pindah_pohon
     @pohon = Pohon.find(params[:id])
     if @pohon.update(pohon_sub_tema_params)
@@ -157,6 +102,7 @@ class PohonTematikController < ApplicationController
 
   def edit_strategi
     @pohon = Pohon.find(params[:id])
+    @opds = Opd.opd_resmi
     @opd = @pohon.opd
     @strategis = @opd.strategis.where(tahun: @pohon.tahun, role: %w[eselon_2 strategi_pohon_kota])
                      .collect { |str| [str.strategi, str.id] }
--- spec/features/pohon_tematik_spec.rb
+++ spec/features/pohon_tematik_spec.rb
@@ -165,7 +165,7 @@ RSpec.feature "PohonTematiks", type: :feature do
       expect(page).to have_content 'ket sub sub'
     end
 
-    scenario 'strategic dibawah', :js do
+    scenario 'strategic dibawah sub sub tematik', :js do
       within all('.pohon-foot').last do
         click_on "Tampilkan"
         click_on "Strategi Tematik Baru"
@@ -185,6 +185,87 @@ RSpec.feature "PohonTematiks", type: :feature do
       click_button "Ok"
 
       expect(page).to have_content 'strategi a'
+    end
+  end
+
+  context 'strategic tematik' do
+    before(:each) do
+      tema = create(:tematik, tema: 'test tematik 1',
+                              keterangan: 'test tema')
+
+      sub_tema = create(:tematik, tema: 'test sub tematik',
+                                  type: 'SubTematik',
+                                  tematik_ref_id: tema.id,
+                                  keterangan: 'test sub tema')
+
+      # sub_sub_tema = create(:tematik, tema: 'test sub sub tematik',
+      #                                 type: 'SubSubTematik',
+      #                                 tematik_ref_id: sub_tema.id,
+      #                                 keterangan: 'test sub tema')
+      strategic = create(:strategi,
+                         strategi: 'test strategi',
+                         type: 'StrategiPohon',
+                         tahun: '2023')
+
+      pohon_tema = create(:pohon, pohonable_type: 'Tematik',
+                                  pohonable_id: tema.id,
+                                  tahun: '2023',
+                                  keterangan: 'test pohon tema',
+                                  role: 'pohon_kota')
+
+      pohon_sub = create(:pohon, pohonable_type: 'SubTematik',
+                                 pohonable_id: sub_tema.id,
+                                 pohon_ref_id: pohon_tema.id,
+                                 keterangan: 'test pohon sub',
+                                 tahun: '2023',
+                                 role: 'sub_pohon_kota')
+
+      # pohon_sub_sub = create(:pohon, pohonable_type: 'SubSubTematik',
+      #                                pohonable_id: sub_sub_tema.id,
+      #                                pohon_ref_id: pohon_sub.id,
+      #                                keterangan: 'test sub sub',
+      #                                tahun: '2023',
+      #                                role: 'sub_sub_pohon_kota')
+      create(:pohon,
+             pohonable_type: 'Strategi',
+             pohonable_id: strategic.id,
+             role: 'strategi_pohon_kota',
+             keterangan: 'pohon strategi 1',
+             pohon_ref_id: pohon_sub.id,
+             opd: user.opd,
+             tahun: '2023')
+
+      visit kota_pohon_kinerja_index_path
+
+      within('.card-body') do
+        select2 'test tematik 1', from: 'Tematik', search: 'test'
+        click_on "Filter"
+      end
+
+      click_on "Tampilkan"
+    end
+
+    scenario 'edit strategic', :js do
+      within all('.pohon-foot').last do
+        click_on "Tampilkan"
+      end
+
+      within all('.pohon-tombol').last do
+        click_on "Edit"
+      end
+
+      within('.form-strategi-tematik') do
+        fill_in 'sub_sub_tematik[tema]', with: 'Test edit strategic'
+        fill_in 'Indikator', with: 'Indikator a'
+        fill_in 'Target', with: '100'
+        fill_in 'Satuan', with: '%'
+        fill_in 'Keterangan', with: 'keterangan sub sub'
+        click_on 'Simpan'
+      end
+
+      click_button "Ok"
+
+      expect(page).to have_content 'Test edit strategic'
     end
   end
 end
