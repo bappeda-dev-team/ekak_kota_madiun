653d5ebb768ffecbb88a942948cfade58bd5c818
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-09-19 08:38:47 +0700

    tampilkan pohon dari kota ke opd

--- app/helpers/pohon_kinerja_opds_helper.rb
+++ app/helpers/pohon_kinerja_opds_helper.rb
@@ -9,8 +9,12 @@ module PohonKinerjaOpdsHelper
     case pohon_kinerja_opd.role
     when 'eselon_2'
       child_strategi(pohon_kinerja_opd, @tactical_opd)
+    when 'strategi_pohon_kota'
+      child_strategi(pohon_kinerja_opd, @tactical_kota)
     when 'eselon_3'
       child_strategi(pohon_kinerja_opd, @operational_opd)
+    when 'tactical_pohon_kota'
+      child_strategi(pohon_kinerja_opd, @operational_kota)
     else
       child_strategi(pohon_kinerja_opd, @staff_opd)
     end
