dde889ef96168a2173a9ba5e7457637cebf198bc
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-09-21 10:19:52 +0700

    disable edit untuk selain eselon 2,3 dan admin

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -32,6 +32,10 @@ module UsersHelper
     current_user.kode_opd != "1234567890"
   end
 
+  def eselon_x?(*roles)
+    current_user.roles.map(&:name).any? { |role| roles.include?(role) }
+  end
+
   def selected_user(user)
     options_for_select([[user.nama, user.nik]], selected: user.nik)
   end
