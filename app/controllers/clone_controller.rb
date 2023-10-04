9ebd0f6958692cd7365d2d2d03e45cd6c47b026e
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-10-04 08:04:05 +0700

    first thing first

--- app/controllers/clone_controller.rb
+++ app/controllers/clone_controller.rb
@@ -6,6 +6,33 @@ class CloneController < ApplicationController
     render partial: 'clone/form_clone', locals: { pohon: pohon, tahun_asal: tahun, url: url }, layout: false
   end
 
+  def opd
+    pohon = Opd.find params[:id]
+    tahun = cookies[:tahun]
+    url = pohon_opd_clone_path(params[:id])
+    render partial: 'clone/form_clone', locals: { pohon: pohon, tahun_asal: tahun, url: url }, layout: false
+  end
+
+  def pohon_opd
+    tahun_asal = params[:tahun_asal]
+    tahun_anggaran = KelompokAnggaran.find(params[:tahun_tujuan]).kode_kelompok
+    @tahun = tahun_anggaran.match(/murni/) ? tahun_anggaran[/[^_]\d*/, 0] : tahun_anggaran
+
+    pohon = Pohon.find(params[:id])
+
+    ket = "clone_dari_#{tahun_asal}"
+
+    operation = PohonCloner.call(pohon, traits: :pohon_tematik,
+                                        tahun: @tahun, pohon_ref_id: '', keterangan: ket)
+    operation.to_record
+    if operation.persist!
+      clone_pohon = operation.to_record
+      render json: { resText: "Pohon di clone ke #{clone_pohon.tahun}" }, status: :created
+    else
+      render json: { resText: 'Gagal, terdapat kesalahan di server' }, status: :unprocessable_entity
+    end
+  end
+
   def pohon_tematik
     tahun_asal = params[:tahun_asal]
     tahun_anggaran = KelompokAnggaran.find(params[:tahun_tujuan]).kode_kelompok
