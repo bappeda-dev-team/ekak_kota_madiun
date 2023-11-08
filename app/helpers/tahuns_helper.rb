ca6410870cb0d15be47df7b87c7d29e92cc9e969
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-11-08 11:56:09 +0700

    indikator tujuan in opd

--- app/helpers/tahuns_helper.rb
+++ app/helpers/tahuns_helper.rb
@@ -2,4 +2,8 @@ module TahunsHelper
   def selected_tahun(tahun)
     options_for_select([[tahun, tahun]], selected: tahun)
   end
+
+  def tahun_without_perubahan(tahun)
+    tahun.gsub('_perubahan', '')
+  end
 end
