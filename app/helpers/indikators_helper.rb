b66e43b02638761d4fa4ba5acf57800f370a4e4a
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-12-05 12:43:12 +0700

    cleanup view rkpd lagi

--- app/helpers/indikators_helper.rb
+++ app/helpers/indikators_helper.rb
@@ -1,5 +1,5 @@
 module IndikatorsHelper
   def ind_prg(program, tahun, kode_opd)
-    program.indikator_program_tahun(tahun, kode_opd)
+    indikator_program = program.indikator_program_tahun(tahun, kode_opd)
   end
 end
