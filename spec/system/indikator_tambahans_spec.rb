33c8736c9c143e1e1c18245ed36fb11309d87441
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-12-06 08:14:55 +0700

    spec indikator lppd

--- spec/system/indikator_tambahans_spec.rb
+++ spec/system/indikator_tambahans_spec.rb
@@ -11,11 +11,69 @@ RSpec.describe "IndikatorTambahans", type: :system do
     create_cookie('tahun', '2025')
   end
 
-  context 'indikator rkpd' do
-    it 'should show tematik kota, sub_tematik and their indikator' do
-      visit rkpd_indikators_path
+  context 'indikator lppd outcome opd' do
+    it 'create new lppd outcome' do
+      visit lppd_outcome_indikators_path
 
-      expect(page).to have_text('Tematik A')
+      click_on 'Indikator Outcome LPPD'
+
+      fill_in 'Indikator', with: 'Indikator test'
+      fill_in 'Target', with: '10'
+      fill_in 'Satuan', with: '%'
+      fill_in 'Keterangan', with: 'keterangan a'
+
+      click_on 'Simpan Indikator'
+
+      expect(page).to have_content('Indikator test')
+    end
+
+    it 'edit lppd outcome' do
+      create(:indikator, indikator: 'test', jenis: 'LPPD', sub_jenis: 'Outcome', kode_opd: 'test_opd', tahun: '2025')
+
+      visit lppd_outcome_indikators_path
+
+      expect(page).to have_content('test')
+      # edit new item
+      click_on 'Edit'
+
+      fill_in 'Indikator', with: 'Indikator edit'
+
+      click_on 'Simpan Perubahan Indikator'
+
+      expect(page).to have_content('Indikator edit')
+    end
+  end
+
+  context 'indikator lppd output opd' do
+    it 'create new lppd outcome' do
+      visit lppd_output_indikators_path
+
+      click_on 'Indikator Output LPPD'
+
+      fill_in 'Indikator', with: 'Indikator test'
+      fill_in 'Target', with: '10'
+      fill_in 'Satuan', with: '%'
+      fill_in 'Keterangan', with: 'keterangan a'
+
+      click_on 'Simpan Indikator'
+
+      expect(page).to have_content('Indikator test')
+    end
+
+    it 'edit lppd outcome' do
+      create(:indikator, indikator: 'test', jenis: 'LPPD', sub_jenis: 'Output', kode_opd: 'test_opd', tahun: '2025')
+
+      visit lppd_output_indikators_path
+
+      expect(page).to have_content('test')
+      # edit new item
+      click_on 'Edit'
+
+      fill_in 'Indikator', with: 'Indikator edit'
+
+      click_on 'Simpan Perubahan Indikator'
+
+      expect(page).to have_content('Indikator edit')
     end
   end
 end
