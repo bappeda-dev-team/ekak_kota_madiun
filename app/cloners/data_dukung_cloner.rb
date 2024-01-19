881e77cbbd5aa78937cd11fcc908f2cc011a253e
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-01-19 10:12:06 +0700

    clone with data dukung + jumlah

--- app/cloners/data_dukung_cloner.rb
+++ app/cloners/data_dukung_cloner.rb
@@ -1,9 +1,10 @@
 class DataDukungCloner < Clowne::Cloner
   adapter :active_record
 
-  finalize do |_source, record, tahun:, tahun_asal:, **|
+  include_association :jumlahs
+
+  finalize do |_source, record, tahun:, **|
     record.tahun = tahun
-    record.keterangan = "clone_#{tahun_asal}"
     record.created_at = Time.current
     record.updated_at = Time.current
   end
--- app/cloners/permasalahan_opd_cloner.rb
+++ app/cloners/permasalahan_opd_cloner.rb
@@ -1,6 +1,8 @@
 class PermasalahanOpdCloner < Clowne::Cloner
   adapter :active_record
 
+  include_association :data_dukungs, params: true
+
   finalize do |_source, record, tahun:, **|
     record.tahun = tahun
     record.created_at = Time.current
