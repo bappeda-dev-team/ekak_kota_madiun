3ff9d4aa4e71e02fa18c9bbcc620521c82f07fc3
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-01-22 18:04:24 +0700

    indikator spm opd

--- app/controllers/laporans_controller.rb
+++ app/controllers/laporans_controller.rb
@@ -189,7 +189,14 @@ class LaporansController < ApplicationController
     @lppd_output = @opd.lppd_output.where(tahun: @tahun)
   end
 
-  def indikator_spm; end
+  def indikator_spm
+    @tahun = cookies[:tahun]
+    @kode_opd = cookies[:opd]
+    @opd = Opd.find_by(kode_unik_opd: @kode_opd)
+    @nama_opd = @opd.nama_opd
+    @spm_outcome = @opd.spm_outcome.where(tahun: @tahun)
+    @spm_output = @opd.spm_output.where(tahun: @tahun)
+  end
 
   def indikator_sdgs; end
 
