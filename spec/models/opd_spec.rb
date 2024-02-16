bbccc7a5b2c5a8c0bcee07e243b5db580c28ce4e
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-02-17 01:29:38 +0700

    fix opd spec

--- spec/models/opd_spec.rb
+++ spec/models/opd_spec.rb
@@ -33,8 +33,7 @@ require 'rails_helper'
 
 RSpec.describe Opd, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:nama_opd)  }
-    it { should validate_presence_of(:kode_opd)  }
+    it { should validate_presence_of(:nama_opd) }
   end
 
   describe 'urusan dan bidang urusan' do
