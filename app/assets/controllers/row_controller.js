8cba8fd4e4a8def66cc618e79d74479050401c13
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-02-18 20:52:31 +0700

    cleanup ..

--- app/assets/controllers/row_controller.js
+++ app/assets/controllers/row_controller.js
@@ -1,20 +1,7 @@
-import Rails from "@rails/ujs";
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [
-    "table",
-    "anggaran",
-    "penetapan",
-    "cancel",
-    "simpan",
-    "formInput",
-    "hapus",
-    "form",
-    "main",
-  ];
   static values = {
-    anggaran: Number,
     element: String
   };
 
@@ -59,12 +46,12 @@ export default class extends Controller {
 
     if (status == "Unprocessable Entity") {
       // targetRow.outerHTML = html_content;
-      console.log('error xx', resText)
+      // alert error here
+      console.error({ error: resText })
     } else {
       targetRow.outerHTML = html_content;
       this.animateBackground(targetRow);
     }
-    // this.sweetalertStatus(resText, status);
   }
 
   animateBackground(target) {
@@ -81,74 +68,5 @@ export default class extends Controller {
       ],
       10000,
     );
-  }
-
-  closeForm() {
-    const formElement = this.formTarget;
-    const mainElement = this.mainTarget;
-    formElement.classList.toggle("d-none");
-    formElement.innerHTML = "";
-    mainElement.classList.toggle("d-none");
-  }
-
-  simpan() {
-    this.toggleButton();
-    const form = this.formTarget.querySelector("form");
-    Rails.fire(form, "submit");
-  }
-
-  toggleButton() {
-    this.cancelTarget.classList.add("d-none");
-    this.simpanTarget.classList.add("d-none");
-    this.editTarget.classList.remove("d-none");
-    this.hapusTarget.classList.remove("d-none");
-    this.penetapanTarget.classList.remove("d-none");
-    this.formTarget.classList.add("d-none");
-  }
-
-  onPostSuccess(event) {
-    const result = event.detail;
-    const [data, _status, _request] = result;
-
-    const { anggaran, total, parent, jumlah } = data.results;
-    this.penetapanTarget.innerHTML = anggaran;
-    const totalPenetapan = document.getElementById(parent);
-    const jumlahPenetapan = document.getElementById("totalPenetapan");
-    totalPenetapan.innerHTML = total;
-    jumlahPenetapan.innerHTML = jumlah;
-    this.formTarget.innerHTML = "";
-    this.element.classList.add("higlighted");
-  }
-
-  sweetalertStatus(text, status) {
-    if (status == "Accepted") {
-      Swal.fire({
-        title: "Sukses",
-        text: text,
-        icon: "success",
-        confirmButtonText: "Ok",
-      });
-    } else if (status == "OK") {
-      Swal.fire({
-        title: "Sukses",
-        text: text,
-        icon: "success",
-        confirmButtonText: "Ok",
-      });
-    } else if (status == "Created") {
-      Swal.fire({
-        title: "Sukses",
-        text: text,
-        icon: "success",
-        confirmButtonText: "Ok",
-      });
-    } else {
-      Swal.fire({
-        title: "Gagal",
-        text: text,
-        icon: "error",
-        confirmButtonText: "Ok",
-      });
-    }
   }
 }
