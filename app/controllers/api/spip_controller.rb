b34a44050cd245655de1ff0eb0416071190c8b90
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-05-06 12:13:09 +0700

    sasaran program dan indikator

--- app/controllers/api/spip_controller.rb
+++ app/controllers/api/spip_controller.rb
@@ -20,13 +20,15 @@ module Api
       program_kegiatans = ProgramKegiatan.where(kode_sub_skpd: kode_opd)
                                          .where.not(kode_skpd: [nil, ""])
       items = program_kegiatans.map do |pr|
+        indikator_programs = indikators(pr.kode_program, 'Program', pr.kode_sub_skpd, tahun)
+        sasaran_programs = sasaran_program(pr, tahun)
         { jenis: 'program',
           kode_opd: pr.kode_sub_skpd,
           kode: pr.kode_program,
           nama: pr.nama_program,
           pagu: 0,
-          indikators: indikators(pr.kode_program, 'Program', pr.kode_sub_skpd, tahun)
-        }
+          indikators: indikator_programs,
+          sasaran_program: sasaran_programs }
       end
       items.uniq { |pk| pk.values_at(:kode, :kode_opd) }.sort_by { |pk| pk.values_at(:kode) }
     end
@@ -45,6 +47,36 @@ module Api
         target: indikator&.target,
         satuan: indikator&.satuan
       }
+    end
+
+    def sasaran_program(program_kegiatan, tahun)
+      sasarans = program_kegiatan.sasarans.includes(%i[strategi user])
+                                 .where(tahun: tahun).dengan_strategi
+      sasarans.map do |sasaran|
+        {
+          id: sasaran.id,
+          sasaran: sasaran.sasaran_kinerja,
+          nama: sasaran.user.nama,
+          nip: sasaran.nip_asn,
+          indikator_sasarans: indikator_sasaran(sasaran),
+          strategi: sasaran.strategi.to_s,
+          role: sasaran.strategi.role,
+          jabatan: sasaran.user.nama_jabatan_terakhir
+        }
+      end
+    end
+
+
+    def indikator_sasaran(sasaran)
+      indikators = sasaran.indikator_sasarans
+      indikators.map do |indikator|
+        {
+          id: indikator.id,
+          indikator: indikator.indikator_kinerja,
+          target: indikator.target,
+          satuan: indikator.satuan
+        }
+      end
     end
   end
 end
