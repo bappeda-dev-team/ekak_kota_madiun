b30c846641025900ff772ae635057ae4c549c5d3
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-05-14 15:35:12 +0700

    fix daftar resiko helper

--- app/helpers/sasaran_program_opds_helper.rb
+++ app/helpers/sasaran_program_opds_helper.rb
@@ -2,9 +2,9 @@ module SasaranProgramOpdsHelper
   def peta_resiko(kemungkinan, skala_dampak)
     matrix_resiko =
       { A: [[1, 1]],
-        B: [[1, 2], [2, 1], [2, 2], [3, 1], [1, 3]],
-        C: [[2, 3], [3, 2], [4, 1], [1, 4], [3, 3]],
-        D: [[4, 2], [2, 4], [3, 4]] }
+        B: [[1, 2], [1, 3], [2, 1], [2, 2], [3, 1]],
+        C: [[1, 4], [2, 3], [3, 2], [4, 1], [3, 3]],
+        D: [[2, 4], [3, 4], [4, 2], [4, 4]] }
     matrik = [kemungkinan.to_i, skala_dampak.to_i]
     nilai = matrix_resiko.select { |_nilai, skor| skor.include?(matrik) }
     nilai.key(nilai.values.flatten(1)).to_s
