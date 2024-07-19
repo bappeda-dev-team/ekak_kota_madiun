16a4a0b5983150dea0795bf8539c4fab13f6a275
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-07-20 06:40:30 +0700

    fix typo

--- spec/components/renstra_row_component_spec.rb
+++ spec/components/renstra_row_component_spec.rb
@@ -3,14 +3,16 @@
 require "rails_helper"
 
 RSpec.describe RenstraRowComponent, type: :component do
-  it "replace last XX kode with 00XX for subkegiatan" do
-    # Arrange
-    row_copmonent = RenstraRowComponent.new(program: { kode: '4.01.02.2.01.01', jenis: 'subkegiatan' })
+  context 'subkegiatan' do
+    it "replace last XX kode with 00XX for subkegiatan" do
+      # Arrange
+      row_component = RenstraRowComponent.new(program: { kode: '4.01.02.2.01.01', jenis: 'subkegiatan' })
 
-    # Act
-    new_kode = row_copmonent.kode_tweak
+      # Act
+      new_kode = row_component.kode_tweak
 
-    # Assert
-    expect(new_kode).to eq('4.01.02.2.01.0001')
+      # Assert
+      expect(new_kode).to eq('4.01.02.2.01.0001')
+    end
   end
 end
