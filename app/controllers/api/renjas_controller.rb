8d6e2c66d986601d517fc7cd85cc6dc076e5bb71
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-07-24 10:33:57 +0700

    renja rankir api spec

--- app/controllers/api/renjas_controller.rb
+++ app/controllers/api/renjas_controller.rb
@@ -4,11 +4,14 @@ class Api::RenjasController < ActionController::API
   def rankir
     @kode_opd = params[:kode_opd]
     @tahun = params[:tahun]
-    @opd = Opd.find_by(kode_unik_opd: @kode_opd)
+    @opd = Opd.find_by!(kode_unik_opd: @kode_opd)
     @nama_opd = @opd.nama_opd
 
     renja = RenjaService.new(kode_opd: @kode_opd, tahun: @tahun, jenis: 'rankir')
     @program_kegiatans = renja.program_kegiatan_renja
+  rescue ActiveRecord::RecordNotFound => e
+    logger.info e
+    render json: { message: "Opd tidak ditemukan" },
+           status: :not_found
   end
-
 end
--- spec/requests/api/renjas_request_spec.rb
+++ spec/requests/api/renjas_request_spec.rb
@@ -2,17 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Renjas", type: :request do
   describe "GET /rankir.json" do
+    def valid_params
+      kode_opd = FactoryBot.create(:opd).kode_unik_opd
+      tahun = '2025'
+
+      { tahun: tahun, kode_opd: kode_opd, format: :json }
+    end
+
+    def invalid_params
+      { tahun: '', kode_opd: '', format: :json }
+    end
+
     context 'with valid params' do
-      def valid_params
-        kode_opd = FactoryBot.create(:opd).kode_unik_opd
-        tahun = '2025'
-
-        { tahun: tahun, kode_opd: kode_opd, format: :json }
-      end
-
       it "returns http success" do
         get "/api/renjas/rankir.json", params: valid_params
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'returns http not_found' do
+        get '/api/renjas/rankir.json', params: invalid_params
+        expect(response).to have_http_status(:not_found)
       end
     end
   end
