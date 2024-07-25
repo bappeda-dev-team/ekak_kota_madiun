5aaf16583a58439edd7d949c6071801cb59fde5d
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-07-25 17:11:34 +0700

    edit jenis inovasi sasaran

--- spec/features/sasarans_spec.rb
+++ spec/features/sasarans_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Sasarans", type: :feature do
-  let(:user) { create(:eselon_4) }
-
-  before(:each) do
+  scenario "new sasaran with pohon kinerja on top", js: true do
+    user = create(:eselon_4)
     login_as user
 
     # setup cookies
@@ -11,9 +10,7 @@ RSpec.describe "Sasarans", type: :feature do
 
     create_cookie('opd', 'test_opd')
     create_cookie('tahun', '2023')
-  end
 
-  scenario "new sasaran with pohon kinerja on top", js: true do
     visit new_sasaran_path(tipe: user.eselon_user)
     fill_in 'sasaran[sasaran_kinerja]', with: 'Test Rencana Kinerja'
     fill_in 'Indikator kinerja', with: 'Indikator'
@@ -21,5 +18,41 @@ RSpec.describe "Sasarans", type: :feature do
     fill_in 'Satuan', with: '%'
     click_on 'Simpan'
     expect(page).to have_text('Test Rencana Kinerja')
+  end
+
+  include_context 'sasaran_complete'
+
+  scenario 'user(eselon_4) create sasaran with inovasi and edit it', js: true do
+    sasaran_complete
+
+    within('#inovasi-sasaran') do
+      select2('Inovasi', from: 'Hasil inovasi', exact_text: true)
+      fill_in('Inovasi', with: 'inovasi test')
+      select2('Pengembangan', from: 'Jenis inovasi', exact_text: true)
+      fill_in('Gambaran nilai kebaruan', with: 'gambaran kebaruan test')
+      click_on('Simpan Perubahan Sasaran')
+    end
+    click_on('Ok')
+
+    expect(page).to have_css('td.judul-inovasi', text: 'inovasi test')
+    expect(page).to have_css('td.jenis-inovasi', text: 'Pengembangan')
+    expect(page).to have_css('td.gambaran-inovasi', text: 'gambaran kebaruan test')
+
+    refresh
+
+    click_on('Edit Inovasi')
+
+    within('#edit-inovasi-sasaran') do
+      fill_in('Inovasi', with: 'inovasi-ubah-1')
+      select2('Baru', from: 'Jenis inovasi', exact_text: true)
+      fill_in('Gambaran Nilai Kebaruan', with: 'baru-x')
+      click_on('Simpan Perubahan Sasaran')
+    end
+
+    click_on('Ok')
+
+    expect(page).to have_css('td.judul-inovasi', text: 'inovasi-ubah-1')
+    expect(page).to have_css('td.jenis-inovasi', text: 'Baru')
+    expect(page).to have_css('td.gambaran-inovasi', text: 'baru-x')
   end
 end
