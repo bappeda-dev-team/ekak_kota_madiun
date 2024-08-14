5c573f56c559d060bf9b93f67444a633d2eab244
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-08-14 10:48:50 +0700

    fix skala dampak

--- app/helpers/sasaran_program_opds_helper.rb
+++ app/helpers/sasaran_program_opds_helper.rb
@@ -3,8 +3,8 @@ module SasaranProgramOpdsHelper
     matrix_resiko =
       { A: [[1, 1]],
         B: [[1, 2], [1, 3], [2, 1], [2, 2], [3, 1]],
-        C: [[1, 4], [2, 3], [3, 2], [4, 1], [3, 3]],
-        D: [[2, 4], [3, 4], [4, 2], [4, 4]] }
+        C: [[1, 4], [2, 3], [3, 2], [3, 3], [4, 1]],
+        D: [[2, 4], [3, 4], [4, 2], [4, 3], [4, 4]] }
     matrik = [kemungkinan.to_i, skala_dampak.to_i]
     nilai = matrix_resiko.select { |_nilai, skor| skor.include?(matrik) }
     nilai.key(nilai.values.flatten(1)).to_s
