2d4740bf6f910e643bfc8091c774401158ee6770
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-10-18 17:03:36 +0700

    jiggly disappear

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,22 @@
 class SearchController < ApplicationController
+  before_action :usulan_params
+
   def usulan_user
-    tahun = cookies[:tahun] || Date.current.year.to_s
-    tahun_bener = tahun.match(/murni|perubahan/) ? tahun[/[^_]\d*/, 0] : tahun
     param = params[:q] || ''
-    usulan_type = params[:usulan_type]
-    nip_asn = params[:nip_asn]
     @results = Search::AllUsulan
                .where("sasaran_id is null and usulan ILIKE ?", "%#{param}%")
-               .where(searchable_type: usulan_type.capitalize, nip_asn: nip_asn, tahun: tahun)
+               .where(searchable_type: @usulan_type, nip_asn: @nip_asn, tahun: @tahun)
                .order(searchable_id: :desc)
                .includes(:searchable)
                .collect(&:searchable)
+  end
+
+  private
+
+  def usulan_params
+    @tahun = cookies[:tahun] || Date.current.year.to_s
+    @tahun_bener = @tahun.match(/murni|perubahan/) ? @tahun[/[^_]\d*/, 0] : @tahun
+    @usulan_type = params[:usulan_type].capitalize || ''
+    @nip_asn = params[:nip_asn]
   end
 end
