d5cd8214d7105f703f335bc6ec8ed0ac70331075
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-11-13 16:48:27 +0700

    fix ajax filter pohon tematik

--- app/assets/controllers/filter_render_controller.js
+++ app/assets/controllers/filter_render_controller.js
@@ -1,31 +1,32 @@
-import { Controller } from 'stimulus'
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-        static targets = ['results']
+  static targets = ["results"];
 
-        append(event) {
-                const [_data, _status, xhr] = event.detail;
-                // alert(status)
-                this.resultsTarget.innerHTML = xhr.response
-        }
+  append(event) {
+    const [_data, _status, xhr] = event.detail;
+    // alert(status)
+    this.resultsTarget.innerHTML = xhr.response;
+  }
 
-        warning(event) {
-                const [_data, status, _xhr] = event.detail;
-                alert(status)
-        }
+  warning(event) {
+    const [_data, status, _xhr] = event.detail;
+    alert(status);
+  }
 
-        success(event) {
-                const [xhr, status] = event.detail
-                const target = 'render-results'
-                const element = document.getElementById(target)
+  success(event) {
+    const [, , xhr] = event.detail;
+    const target = "render-results";
+    const element = document.getElementById(target);
+    const html = xhr.response;
+    element.innerHTML = html;
+  }
 
-                if (status == 'OK') {
-                        const html = xhr.response
-                        element.innerHTML = html
-                }
-                else {
-                        const html = '<div class="alert alert-danger" role="alert">Terjadi Kesalahan</div>'
-                        element.innerHTML = html
-                }
-        }
+  error() {
+    const target = "render-results";
+    const element = document.getElementById(target);
+    const html =
+      '<div class="alert alert-danger" role="alert">Terjadi Kesalahan</div>';
+    element.innerHTML = html;
+  }
 }
