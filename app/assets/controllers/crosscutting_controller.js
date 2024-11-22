df6125de0a8ea65a617b96fad1311c42ec160969
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-11-22 07:48:39 +0700

    crosscutting external pemerintah dan non pem

--- app/assets/controllers/crosscutting_controller.js
+++ app/assets/controllers/crosscutting_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["internal", "external", "mitra", "inovasi"];
+  static targets = [
+    "internal",
+    "external",
+    "mitra",
+    "inovasi",
+    "pemerintah",
+    "nonPemerintah",
+    "mitraExternalPemerintah",
+    "inovasiExternalPemerintah",
+    "mitraExternalNonPemerintah",
+    "inovasiExternalNonPemerintah",
+    "ogpExternalNonPemerintah",
+  ];
 
   connect() {
     console.log("controller connected");
@@ -12,7 +24,16 @@ export default class extends Controller {
     if (tipeTerpilih == "Internal") {
       this.internalTarget.classList.toggle("d-none");
     } else {
-      this.exeternalTarget.classList.toggle("d-none");
+      this.externalTarget.classList.toggle("d-none");
+    }
+  }
+
+  aktifkanTipeInstansiTerpilih(event) {
+    const tipeInstansi = event.detail.data.id;
+    if (tipeInstansi == "Pemerintah") {
+      this.pemerintahTarget.classList.toggle("d-none");
+    } else if (tipeInstansi == "Non-Pemerintah") {
+      this.nonPemerintahTarget.classList.toggle("d-none");
     }
   }
 
@@ -20,7 +41,27 @@ export default class extends Controller {
     this.mitraTarget.classList.toggle("d-none");
   }
 
+  inputMitraExternalPemerintah(_event) {
+    this.mitraExternalPemerintahTarget.classList.toggle("d-none");
+  }
+
+  inputMitraExternalNonPemerintah(_event) {
+    this.mitraExternalNonPemerintahTarget.classList.toggle("d-none");
+  }
+
   inputInovasi(_event) {
     this.inovasiTarget.classList.toggle("d-none");
+  }
+
+  inputInovasiExternalPemerintah(_event) {
+    this.inovasiExternalPemerintahTarget.classList.toggle("d-none");
+  }
+
+  inputInovasiExternalNonPemerintah(_event) {
+    this.inovasiExternalNonPemerintahTarget.classList.toggle("d-none");
+  }
+
+  inputOgpExternalNonPemerintah(_event) {
+    this.ogpExternalNonPemerintahTarget.classList.toggle("d-none");
   }
 }
