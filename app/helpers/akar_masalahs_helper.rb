8bc4bcd0c9db060bbdf46f613fec13137a171b78
Ryan B <ryan.brilliant.nirwana@gmail.com>
2024-11-24 17:49:18 +0700

    metaprogramming queries to filter deleted pokin and fix akar masalah for nil thing

--- app/helpers/akar_masalahs_helper.rb
+++ app/helpers/akar_masalahs_helper.rb
@@ -5,7 +5,9 @@ module AkarMasalahsHelper
 
   def rowspan_masalah_pokok(tacticals)
     masalahs = tacticals
-    akar_masalahs = masalahs.flat_map { |tact| tact.strategi_bawahans.size }.reduce(:+)
+    akar_masalahs = masalahs.flat_map { |tact| tact.strategi_bawahans.size }
+                            .reduce(:+).to_i
+
     masalahs.size + akar_masalahs + 1
   end
 
--- app/queries/pohon_kinerja_opd_queries.rb
+++ app/queries/pohon_kinerja_opd_queries.rb
@@ -34,45 +34,46 @@ class PohonKinerjaOpdQueries
   end
   memoize :pohon_crosscutting
 
-  def strategi_kota
-    pohon_kota.rewhere(role: 'strategi_pohon_kota')
+  # dynamic method
+  # Define dynamic methods for each role
+  %w[strategi tactical operational staff].each do |role|
+    # kota
+    define_method("#{role}_kota") do
+      pohon_kota.rewhere(role: "#{role}_pohon_kota")
+    end
+
+    # opd
+    define_method("#{role}_opd") do
+      pohon_opd.rewhere(role: "eselon_#{role_index(role)}")
+               .select { |pp| pp.deleted_at.nil? }
+    end
   end
 
-  def tactical_kota
-    pohon_kota.rewhere(role: 'tactical_pohon_kota')
-  end
-
-  def operational_kota
-    pohon_kota.rewhere(role: 'operational_pohon_kota')
-  end
-
-  def strategi_opd
-    pohon_opd.rewhere(role: 'eselon_2')
-  end
-
-  def tactical_opd
-    pohon_opd.rewhere(role: 'eselon_3')
-  end
-
-  def operational_opd
-    pohon_opd.rewhere(role: 'eselon_4')
-  end
-
-  def staff_opd
-    pohon_opd.rewhere(role: 'staff')
-  end
-
+  # Alias method to pohon_crosscutting
   def strategi_crosscutting
     pohon_crosscutting
   end
 
+  # only get the operational (with subkegiatan)
   def sasaran_operational_opd
     operational_opd.flat_map do |op|
       op.sasarans.includes(:program_kegiatan).dengan_rincian.where(tahun: @tahun)
     end
   end
 
+  # pohon excluding staff
   def all_strategi_opd
     pohon_opd.rewhere(role: %w[eselon_2 eselon_3 eselon_4])
+  end
+
+  private
+
+  def role_index(role)
+    case role
+    when 'strategi' then 2
+    when 'tactical' then 3
+    when 'operational' then 4
+    when 'staff' then nil # Use nil or another index if staff doesn't follow the same pattern
+    end
   end
 end
