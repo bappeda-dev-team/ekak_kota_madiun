0e5d74fb2f2faccb85d638098c6d0a211d52e8e3
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-01-23 07:32:55 +0700

    new level risiko, 3 level

--- app/helpers/sasaran_program_opds_helper.rb
+++ app/helpers/sasaran_program_opds_helper.rb
@@ -1,11 +1,9 @@
 module SasaranProgramOpdsHelper
   def peta_resiko(kemungkinan, skala_dampak)
     matrix_resiko =
-      { A: [[1, 1], [1, 2], [2, 1]],
-        B: [[1, 3], [1, 4], [2, 2], [3, 1], [4, 1]],
-        C: [[1, 5], [2, 3], [2, 4], [3, 2], [3, 3], [4, 2], [5, 1]],
-        D: [[2, 5], [3, 4], [4, 3], [5, 2], [5, 3]],
-        E: [[3, 5], [4, 4], [4, 5], [5, 4], [5, 5]] }
+      { A: [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [3, 1]],
+        B: [[1, 4], [1, 5], [2, 3], [2, 4], [2, 5], [3, 2], [3, 3], [3, 4], [4, 1], [4, 2], [4, 3], [5, 1], [5, 2]],
+        C: [[3, 5], [4, 4], [4, 5], [5, 3], [5, 4], [5, 5]] }
     matrik = [kemungkinan.to_i, skala_dampak.to_i]
     nilai = matrix_resiko.select { |_nilai, skor| skor.include?(matrik) }
     nilai.key(nilai.values.flatten(1)).to_s
@@ -15,14 +13,10 @@ module SasaranProgramOpdsHelper
     prefix = 'Level Risiko'
     case peta_matrix
     when 'A'
-      "#{prefix} Sangat Rendah"
-    when 'B'
       "#{prefix} Rendah"
+    when 'B'
+      "#{prefix} Sedang dan Tinggi"
     when 'C'
-      "#{prefix} Sedang"
-    when 'D'
-      "#{prefix} Tinggi"
-    when 'E'
       "#{prefix} Sangat Tinggi"
     else
       '-'
