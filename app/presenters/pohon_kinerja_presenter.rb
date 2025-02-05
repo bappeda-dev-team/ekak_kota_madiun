3f9fcad36b624ffc6b36afe933ecef3b9decb8e2
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-02-05 10:05:59 +0700

    temp limit pelaksana shown in pokin

--- app/presenters/pohon_kinerja_presenter.rb
+++ app/presenters/pohon_kinerja_presenter.rb
@@ -217,7 +217,7 @@ class PohonKinerjaPresenter
   def pelaksana
     return [] if @pohon.instance_of?(Pohon)
 
-    @pohon.pohon_shareds.where.not(role: %w[opd opd-batal], user_id: nil).order(:user_id)
+    @pohon.pohon_shareds.where.not(role: %w[opd opd-batal], user_id: nil).order(:user_id).limit(10)
   rescue NoMethodError
     []
   end
