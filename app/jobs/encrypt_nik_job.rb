dabb4e894d8e5cfd04728ddb52e0e000f74612f8
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-09-11 13:05:17 +0700

    update agar tidak lock encrypt nik

--- app/jobs/encrypt_nik_job.rb
+++ app/jobs/encrypt_nik_job.rb
@@ -1,8 +1,5 @@
 class EncryptNikJob
-  include Sidekiq::Worker
   include Sidekiq::Job
-  queue_as :default
-  sidekiq_options lock: :until_executed
 
   def perform(nama, nip, nik_asli)
     # request ke server enkripsi
