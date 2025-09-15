bd4b0cef9a1f320b1e656fd59b9eba65feeb8347
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-09-15 14:48:09 +0700

    default host for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,3 +115,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+Rails.application.routes.default_url_options = {
+  host: "kak.madiunkota.go.id",
+  protocol: "https"
+}
