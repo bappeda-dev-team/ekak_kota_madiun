77b8743d390b8fa30f3ca836281c011aa7feb1ce
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-09-19 07:48:14 +0700

    hapus format tanggal alay

--- app/services/user_attr_in_skp.rb
+++ app/services/user_attr_in_skp.rb
@@ -23,8 +23,8 @@ class UserAttrInSkp
         tahun_sasaran: ss.tahun,
         sasaran_kinerja: ss.sasaran_kinerja,
         ada_perubahan: update_dibulan_ini?(ss.updated_at),
-        dibuat: format_tanggal(ss.created_at),
-        update_terakhir: format_tanggal(ss.updated_at),
+        dibuat: ss.created_at,
+        update_terakhir: ss.updated_at,
         terakhir_diubah: terakhir_update(ss.updated_at),
         indikator_sasaran: perubahan_indikator(ss) }
     end
@@ -40,8 +40,8 @@ class UserAttrInSkp
         rumus_perhitungan: ind.rumus_perhitungan,
         target: ind.target,
         satuan: ind.satuan,
-        dibuat: format_tanggal(ind.created_at),
-        update_terakhir: format_tanggal(ind.updated_at),
+        dibuat: ind.created_at,
+        update_terakhir: ind.updated_at,
         terakhir_diubah: terakhir_update(ind.updated_at) }
     end
   end
