634006a0ef45d3a5c88b1c9752b63b25595de8d4
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-10-14 15:34:33 +0700

    rekap bpmn spbe

--- app/helpers/bpmn_spbes_helper.rb
+++ app/helpers/bpmn_spbes_helper.rb
@@ -1,2 +1,16 @@
 module BpmnSpbesHelper
+  def hitung_jumlah_bpmn(bpmn_spbes)
+    bisa_digunakan = bpmn_spbes.count { |b| b.dapat_digunakan_pd_lain }
+    harus_disusun = bpmn_spbes.count { |b| !b.dapat_digunakan_pd_lain }
+
+    sudah_disusun = bpmn_spbes.count { |b| b.file_bpmn.attached? }
+    belum_disusun = bpmn_spbes.count { |b| !b.file_bpmn.attached? }
+
+    {
+      bisa_digunakan_opd_lain: bisa_digunakan,
+      harus_disusun: harus_disusun,
+      sudah_disusun: sudah_disusun,
+      belum_disusun: belum_disusun
+    }
+  end
 end
